46650675e42558797fb931e6afa1d0be9cf98b6a
Ferdinando Villa <ferdinando.villa@gmail.com>
2017-11-09 21:19:52 +0100

    Clean up

--- src/main/java/org/integratedmodelling/klab/package-info.java
+++ src/main/java/org/integratedmodelling/klab/package-info.java
@@ -2,8 +2,9 @@
  * 
  */
 /**
- * The services provided in this package implement the k.LAB environment, available everywhere as singletons. A
- * singleton enum is provided for each of the services in org.integratedmodelling.klab.api.services, such as:
+ * The services provided in this package give full API access to the k.LAB environment through a series of singletons
+ * managers, which are only guaranteed to perform as advertised once an {@link org.integratedmodelling.klab.engine.Engine}
+ * has been started. Each service implements one of the interfaces in org.integratedmodelling.klab.api.services, such as:
  * 
  * <pre>
  * enum Observables implements IObservableService {
@@ -12,13 +13,13 @@
  * }
  * </pre>
  * 
- * so that each service singleton can be referenced as
+ * so that each singleton can be referenced as
  * 
  * <pre>
  * Observables.INSTANCE.method(args);
  * </pre>
  * 
- * Test packages can use dependency injection to swap the runtime to a different implementation as needed.
+ * Test packages can use the singletons or employ dependency injection to inject a different implementation as needed.
  * 
  * @author Ferd
  * @see org.integratedmodelling.klab.api.services
