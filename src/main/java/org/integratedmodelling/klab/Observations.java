70f57307323ca9d1678ee3ba64feb964300081c6
fvilla <ferdinando.villa@gmail.com>
2017-11-14 13:59:40 +0100

    Nothing

--- src/main/java/org/integratedmodelling/klab/Observations.java
+++ src/main/java/org/integratedmodelling/klab/Observations.java
@@ -1,6 +1,8 @@
 package org.integratedmodelling.klab;
 
 import org.integratedmodelling.klab.api.observations.ISubject;
+import org.integratedmodelling.klab.api.runtime.IContext;
+import org.integratedmodelling.klab.api.runtime.ISession;
 import org.integratedmodelling.klab.api.services.IObservationService;
 
 public enum Observations implements IObservationService {
@@ -9,5 +11,10 @@ public enum Observations implements IObservationService {
     public void registerSubjectClass(String concept, Class<? extends ISubject> cls) {
         // TODO Auto-generated method stub
         
+    }
+    
+    @Override
+    public IContext initialize(ISubject observation, ISession session) {
+        return null;
     }
 }
--- src/main/java/org/integratedmodelling/klab/api/services/IObservationService.java
+++ src/main/java/org/integratedmodelling/klab/api/services/IObservationService.java
@@ -1,5 +1,19 @@
 package org.integratedmodelling.klab.api.services;
 
+import org.integratedmodelling.klab.api.observations.ISubject;
+import org.integratedmodelling.klab.api.runtime.IContext;
+import org.integratedmodelling.klab.api.runtime.ISession;
+
 public interface IObservationService {
+
+    /**
+     * The functions that resolves a subject, returning the corresponding
+     * live context.
+     * 
+     * @param observation
+     * @param session 
+     * @return
+     */
+    IContext initialize(ISubject observation, ISession session);
 
 }
