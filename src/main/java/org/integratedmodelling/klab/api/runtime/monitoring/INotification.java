649b5afe95be3c997fe85fac705947b61967c4cb
fvilla <ferdinando.villa@gmail.com>
2017-11-17 17:15:02 +0100

    Nothing

--- src/main/java/org/integratedmodelling/klab/api/runtime/monitoring/INotification.java
+++ src/main/java/org/integratedmodelling/klab/api/runtime/monitoring/INotification.java
@@ -2,8 +2,25 @@ package org.integratedmodelling.klab.api.runtime.monitoring;
 
 public interface INotification {
     
+    // NAAAH replace with action (see provenance) and separate from type
+    public enum Type {
+        created,
+        deletedFile,
+        error,
+        finalizedUpload,
+        importNotes,
+        movedFile,
+        prunedBrokenLink,
+        uploaded,
+        updatedMetadata,
+    }
+    
     long getTimestamp();
     
+    String getUser();
     
+    String getNotes();
 
+    Type getType();
+    
 }
