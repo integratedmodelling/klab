54e202a0db839c0b7f149bdef2543cd79f1c1d4c
Ferdinando Villa <ferdinando.villa@gmail.com>
2017-11-18 21:27:48 +0100

    More API fleshing out.

--- src/main/java/org/integratedmodelling/klab/api/model/IObserver.java
+++ src/main/java/org/integratedmodelling/klab/api/model/IObserver.java
@@ -2,6 +2,13 @@ package org.integratedmodelling.klab.api.model;
 
 import org.integratedmodelling.kim.api.IKimObserver;
 
+/**
+ * An observer is the k.LAB object corresponding to an <code>observe</code> statement
+ * in k.IM.
+ * 
+ * @author Ferd
+ *
+ */
 public interface IObserver extends IKimObserver {
 
 }
--- src/main/java/org/integratedmodelling/klab/kim/ObservationBuilder.java
+++ src/main/java/org/integratedmodelling/klab/kim/ObservationBuilder.java
@@ -1,5 +1,23 @@
 package org.integratedmodelling.klab.kim;
 
+import org.integratedmodelling.kim.model.KimObserver;
+import org.integratedmodelling.klab.api.model.INamespace;
+import org.integratedmodelling.klab.api.model.IObserver;
+import org.integratedmodelling.klab.api.runtime.monitoring.IMonitor;
+
 public enum ObservationBuilder {
+    
     INSTANCE;
+    
+    /**
+     * Build a model from its parsed, syntax-error-free representation coming
+     * from the Kim validator. May add logical errors, which are reported back when this
+     * is called from an intelligent editor.
+     * 
+     * @param model
+     * @return
+     */
+    public IObserver build(KimObserver model, INamespace namespace, IMonitor monitor) {
+        return null;
+    }
 }
