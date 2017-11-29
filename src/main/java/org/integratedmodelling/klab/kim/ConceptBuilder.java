94c276cd38aa2b4964c77fdab2bf7adc20fd19ac
Ferdinando Villa <ferdinando.villa@gmail.com>
2017-11-29 21:27:16 +0100

    Stubs

--- src/main/java/org/integratedmodelling/klab/kim/ConceptBuilder.java
+++ src/main/java/org/integratedmodelling/klab/kim/ConceptBuilder.java
@@ -1,5 +1,6 @@
 package org.integratedmodelling.klab.kim;
 
+import org.integratedmodelling.kim.api.IKimConcept;
 import org.integratedmodelling.kim.api.IKimConceptStatement;
 import org.integratedmodelling.klab.api.knowledge.IConcept;
 import org.integratedmodelling.klab.api.model.INamespace;
@@ -10,6 +11,24 @@ public enum ConceptBuilder {
     INSTANCE;
     
     public IConcept build(IKimConceptStatement concept, INamespace namespace, IMonitor monitor) {
+        
+        IConcept main = null;
+        
+        if (concept.getName() != null) {
+            
+        }
+        
+        return null;
+    }
+    
+    public IConcept declare(IKimConcept concept, IMonitor monitor) {
+        
+        IConcept main = null;
+        
+        if (concept.getName() != null) {
+            
+        }
+        
         return null;
     }
 }
