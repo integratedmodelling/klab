e191476e51278600ffedb507061d0aced34bcabb
fvilla <ferdinando.villa@gmail.com>
2017-11-30 20:21:43 +0100

    More stubs

--- src/main/java/org/integratedmodelling/klab/kim/ConceptBuilder.java
+++ src/main/java/org/integratedmodelling/klab/kim/ConceptBuilder.java
@@ -1,34 +1,50 @@
 package org.integratedmodelling.klab.kim;
 
 import org.integratedmodelling.kim.api.IKimConcept;
+import org.integratedmodelling.kim.api.IKimConcept.Type;
 import org.integratedmodelling.kim.api.IKimConceptStatement;
 import org.integratedmodelling.klab.api.knowledge.IConcept;
 import org.integratedmodelling.klab.api.model.INamespace;
 import org.integratedmodelling.klab.api.runtime.monitoring.IMonitor;
 
 public enum ConceptBuilder {
-    
-    INSTANCE;
-    
-    public IConcept build(IKimConceptStatement concept, INamespace namespace, IMonitor monitor) {
-        
-        IConcept main = null;
-        
-        if (concept.getName() != null) {
-            
-        }
-        
-        return null;
-    }
-    
-    public IConcept declare(IKimConcept concept, IMonitor monitor) {
-        
-        IConcept main = null;
-        
-        if (concept.getName() != null) {
-            
-        }
-        
-        return null;
-    }
+
+	INSTANCE;
+
+	public IConcept build(IKimConceptStatement concept, INamespace namespace, IMonitor monitor) {
+		return build(concept, namespace, null, monitor);
+	}
+
+	public IConcept build(IKimConceptStatement concept, INamespace namespace, IConcept parent, IMonitor monitor) {
+
+		IConcept main = null;
+
+		/*
+		 * main type
+		 */
+		if (concept.getName() != null) {
+
+		}
+
+		/*
+		 * parent
+		 */
+
+		return null;
+	}
+
+	public IConcept declare(IKimConcept concept, IMonitor monitor) {
+
+		IConcept main = null;
+
+		if (concept.getName() != null) {
+
+		}
+
+		return null;
+	}
+
+	public IConcept getCoreType(Type type) {
+		return null;
+	}
 }
