848d7b6f8344257f81b885a9a01b96c43cad9950
fvilla <ferdinando.villa@gmail.com>
2017-12-16 15:48:43 +0100

    Parsing KDL beans from various resources

--- src/main/java/org/integratedmodelling/klab/Dataflows.java
+++ src/main/java/org/integratedmodelling/klab/Dataflows.java
@@ -1,9 +1,20 @@
 package org.integratedmodelling.klab;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
 import org.eclipse.xtext.testing.IInjectorProvider;
 import org.eclipse.xtext.testing.util.ParseHelper;
-import org.integratedmodelling.kdl.kdl.Dataflow;
+import org.integratedmodelling.kdl.api.IKdlDataflow;
+import org.integratedmodelling.kdl.kdl.Model;
+import org.integratedmodelling.kdl.model.Kdl;
 import org.integratedmodelling.klab.api.services.IDataflowService;
+import org.integratedmodelling.klab.exceptions.KlabException;
+import org.integratedmodelling.klab.exceptions.KlabIOException;
+import org.integratedmodelling.klab.exceptions.KlabValidationException;
 import org.integratedmodelling.klab.utils.xtext.DataflowInjectorProvider;
 
 import com.google.inject.Inject;
@@ -12,16 +23,44 @@ import com.google.inject.Injector;
 public enum Dataflows implements IDataflowService {
 
     INSTANCE;
-    
+
     @Inject
-    ParseHelper<Dataflow> dataflowParser;
-    
+    ParseHelper<Model> dataflowParser;
+
     private Dataflows() {
         IInjectorProvider injectorProvider = new DataflowInjectorProvider();
         Injector injector = injectorProvider.getInjector();
         if (injector != null) {
             injector.injectMembers(this);
         }
+    }
+
+    public IKdlDataflow declare(URL url) throws KlabException {
+        try (InputStream stream = url.openStream()) {
+            return declare(stream);
+        } catch (Exception e) {
+            throw new KlabIOException(e);
+        }
+    }    
+    
+    public IKdlDataflow declare(File file) throws KlabException {
+        try (InputStream stream = new FileInputStream(file)) {
+            return declare(stream);
+        } catch (Exception e) {
+            throw new KlabIOException(e);
+        }
+    }    
+    
+    public IKdlDataflow declare(InputStream file) throws KlabValidationException {
+        IKdlDataflow ret = null;
+        try {
+            String definition = IOUtils.toString(file); 
+            Model model = dataflowParser.parse(definition);
+            ret = Kdl.INSTANCE.declare(model);
+        } catch (Exception e) {
+            throw new KlabValidationException(e);
+        }
+        return ret;
     }
 
 }
---
+++ src/main/java/org/integratedmodelling/klab/resolution/Dataflow.java
@@ -0,0 +1,52 @@
+package org.integratedmodelling.klab.resolution;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.integratedmodelling.klab.api.runtime.dataflow.IActuator;
+import org.integratedmodelling.klab.api.runtime.dataflow.IDataflow;
+import org.integratedmodelling.klab.api.runtime.dataflow.ILink;
+import org.integratedmodelling.klab.api.runtime.dataflow.IPort;
+
+public class Dataflow implements IDataflow {
+
+    
+    
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<IPort> getInputs() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<IPort> getOutputs() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<IActuator> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<IActuator> getActuators() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<ILink> getLinks() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
