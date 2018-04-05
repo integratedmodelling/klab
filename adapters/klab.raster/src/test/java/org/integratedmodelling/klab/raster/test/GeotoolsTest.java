0101e87f19c449049e72b1545a587f25930a7647
Ferdinando Villa <ferdinando.villa@gmail.com>
2018-04-05 15:49:00 +0200

    Testing WCS/Geotools before attempting a new implementation

--- adapters/klab.raster/src/test/java/org/integratedmodelling/klab/raster/test/GeotoolsTest.java
+++ adapters/klab.raster/src/test/java/org/integratedmodelling/klab/raster/test/GeotoolsTest.java
@@ -2,35 +2,36 @@ package org.integratedmodelling.klab.raster.test;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Map;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.geotools.wcs.WCSConfiguration;
 import org.geotools.xml.Parser;
+import org.integratedmodelling.klab.utils.collections.Collections;
 import org.xml.sax.SAXException;
 
 import net.opengis.wcs11.GetCapabilitiesType;
-import net.opengis.wcs20.Wcs20Factory;
-import net.opengis.wcs20.impl.Wcs20FactoryImpl;
 
 
 public class GeotoolsTest {
 	public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
-		String baseurl = "https://www.wcs.nrw.de/geobasis/wcs_nw_dgm?service=wcs&request=getcapabilities";
+//		String baseurl = "https://www.wcs.nrw.de/geobasis/wcs_nw_dgm?service=wcs&request=getcapabilities";
+		String baseurl = "http://www.integratedmodelling.org/geoserver/wcs?request=getcapabilities";
 		GeotoolsTest me = new GeotoolsTest();
 		me.getCapabilitiesType(baseurl);
-
 	}
 
 	GetCapabilitiesType getCapabilitiesType(String capRequestPath)
 			throws IOException, SAXException, ParserConfigurationException {
 		Parser parser = new Parser(new WCSConfiguration());
 		URL url = new URL(capRequestPath);
-		Object capabilitiesType = parser.parse(url.openStream());
+		Map<?,?> capabilitiesType = (Map<?,?>)parser.parse(url.openStream());
+		System.out.println(Collections.printAsJson(capabilitiesType));
 		
-		Wcs20Factory factory = Wcs20FactoryImpl.init();
-		net.opengis.wcs20.GetCapabilitiesType wcsGetCapabilities = factory.createGetCapabilitiesType();
-		wcsGetCapabilities.setBaseUrl(capRequestPath); 
+//		Wcs20Factory factory = Wcs20FactoryImpl.init();
+//		net.opengis.wcs20.GetCapabilitiesType wcsGetCapabilities = factory.createGetCapabilitiesType();
+//		wcsGetCapabilities.setBaseUrl(capRequestPath); 
 		
 		return null;
 	}
--- klab.core/src/main/java/org/integratedmodelling/klab/utils/collections/Collections.java
+++ klab.core/src/main/java/org/integratedmodelling/klab/utils/collections/Collections.java
@@ -2,22 +2,54 @@ package org.integratedmodelling.klab.utils.collections;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class Collections {
 
-    /**
-     * Pass any number of lists and return one with all the elements.
-     * @param lists
-     * @return a single list with the content of all those passed
-     */
-    @SafeVarargs
-    public static <T> List<T> join(List<T>... lists) {
-        List<T> ret = new ArrayList<>();
-        if (lists != null) {
-            for (List<T> list : lists) {
-                ret.addAll(list);
-            }
-        }
-        return ret;
-    }
+	/**
+	 * Pass any number of lists and return one with all the elements.
+	 * 
+	 * @param lists
+	 * @return a single list with the content of all those passed
+	 */
+	@SafeVarargs
+	public static <T> List<T> join(List<T>... lists) {
+		List<T> ret = new ArrayList<>();
+		if (lists != null) {
+			for (List<T> list : lists) {
+				ret.addAll(list);
+			}
+		}
+		return ret;
+	}
+
+	/**
+	 * Pretty-print the passed map as a JSON object.
+	 * 
+	 * @param map
+	 * @return
+	 */
+	public static String printAsJson(Map<?, ?> map) {
+
+		ObjectMapper om = new ObjectMapper();
+		om.enable(SerializationFeature.INDENT_OUTPUT); // pretty print
+		om.enable(SerializationFeature.WRITE_NULL_MAP_VALUES); // pretty print
+		om.enable(SerializationFeature.WRITE_SINGLE_ELEM_ARRAYS_UNWRAPPED); // pretty print
+
+//		DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+//		prettyPrinter.indentArraysWith(DefaultPrettyPrinter.Lf2SpacesIndenter.instance);
+//
+//		String json = objectMapper.writer(prettyPrinter).writeValueAsString(object);
+
+		try {
+			return om.writeValueAsString(map);
+		} catch (JsonProcessingException e) {
+			throw new IllegalArgumentException("serialization failed");
+		}
+	}
 }
