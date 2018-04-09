d98b72e5a3893c07bce54cef1a4106af6dc84258
fvilla <ferdinando.villa@gmail.com>
2018-04-09 17:23:32 +0200

    Beginning of raster file validator

--- adapters/klab.raster/src/main/java/org/integratedmodelling/klab/raster/files/RasterValidator.java
+++ adapters/klab.raster/src/main/java/org/integratedmodelling/klab/raster/files/RasterValidator.java
@@ -8,28 +8,40 @@ import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.coverage.grid.io.GridFormatFinder;
 import org.integratedmodelling.kim.api.IParameters;
+import org.integratedmodelling.klab.Resources;
 import org.integratedmodelling.klab.api.data.IResource;
 import org.integratedmodelling.klab.api.data.adapters.IResourceValidator;
+import org.integratedmodelling.klab.utils.URLUtils;
 import org.opengis.geometry.Envelope;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 public class RasterValidator implements IResourceValidator {
-  
+
   @Override
   public IResource.Builder validate(URL url, IParameters userData) {
-    return null;
-  }
-
-  private Envelope getEnvelope(File file) throws IOException {
-    Envelope envelope = null;
+    
+    IResource.Builder ret = Resources.INSTANCE.createResourceBuilder();
+    
     try {
-        AbstractGridFormat format = GridFormatFinder.findFormat(file);
-        AbstractGridCoverage2DReader reader = format.getReader(file);
-        GridCoverage2D coverage = reader.read(null);
-        // TODO fill out metadata
-        envelope = coverage.getEnvelope();
-    } catch (NullPointerException e) {
-        return null;
+      File file = URLUtils.getFileForURL(url);
+      AbstractGridFormat format = GridFormatFinder.findFormat(file);
+      AbstractGridCoverage2DReader reader = format.getReader(file);
+      GridCoverage2D coverage = reader.read(null);
+      
+      Envelope envelope = coverage.getEnvelope();
+      // TODO check envelope
+      
+      CoordinateReferenceSystem crs = coverage.getCoordinateReferenceSystem();
+      
+      // TODO crs must be not null, known and parseable by k.LAB
+      // TODO stated nodata
+      // TODO encode extents and resolution
+      
+    } catch (Exception e) {
+      // TODO add errors to builder
+      return null;
     }
-    return envelope;
-}
+    
+    return ret;
+  }
 }
