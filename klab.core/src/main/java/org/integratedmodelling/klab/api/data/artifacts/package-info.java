c4b0357f7f5248a8149c9336cf5abe42b871b953
fvilla <ferdinando.villa@gmail.com>
2018-04-12 13:02:27 +0200

    Configuring in Kotlin support in klab.core and klab.engine

--- klab.core/src/main/java/org/integratedmodelling/klab/api/data/artifacts/package-info.java
+++ klab.core/src/main/java/org/integratedmodelling/klab/api/data/artifacts/package-info.java
@@ -3,7 +3,7 @@
  */
 /**
  * This package specifies the API for the raw data objects produced and manipulated by
- * {@link IContextualizer contextualizers}. These objects are "dressed" with semantics and
- * provenance API by the k.LAB engine to produce observational artifacts.
+ * contextualizers. These objects are "dressed" with semantics and provenance API by the k.LAB
+ * engine to produce observational artifacts.
  */
 package org.integratedmodelling.klab.api.data.artifacts;
---
+++ klab.core/src/main/java/org/integratedmodelling/klab/data/rest/resources/ObservationRequest.kt
@@ -0,0 +1,5 @@
+package org.integratedmodelling.klab.data.rest.resources
+
+class ObservationRequest {
+	var name: String? = null
+}
