f59c7fe40e0848f4f51b488576406d5bdbec35a5
Enrico Girotto <enrico.girotto@gmail.com>
2018-06-28 13:34:18 +0200

    Need to have this during separate front end development
    Needed to call rest service from another origin (different port)
    When it will no longer be necessary, it will be removed

--- klab.engine/src/main/java/org/integratedmodelling/klab/engine/rest/controllers/engine/EngineViewController.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/engine/rest/controllers/engine/EngineViewController.java
@@ -26,6 +26,7 @@ import org.integratedmodelling.klab.rest.StateSummary;
 import org.integratedmodelling.klab.utils.NumberUtils;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,6 +43,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @Secured(Roles.SESSION)
 public class EngineViewController {
 
