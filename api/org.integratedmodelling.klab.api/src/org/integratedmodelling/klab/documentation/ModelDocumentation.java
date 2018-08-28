32740ddbc80f96c88745a8b6965321c3c8923c0d
fvilla <ferdinando.villa@gmail.com>
2018-08-28 14:05:49 +0200

    Test commit for previous workspace disaster

--- api/org.integratedmodelling.klab.api/src/org/integratedmodelling/klab/documentation/ModelDocumentation.java
+++ api/org.integratedmodelling.klab.api/src/org/integratedmodelling/klab/documentation/ModelDocumentation.java
@@ -7,32 +7,59 @@ import org.integratedmodelling.klab.rest.AttributeReference;
 
 public class ModelDocumentation {
 
-    private String                          extended;
-    private String                          template;
-    private Map<String, AttributeReference> variables = new HashMap<>();
+	private String trigger;
+	private String section;
+	private String documentedId;
+	private String extended;
+	private String template;
+	private Map<String, AttributeReference> variables = new HashMap<>();
 
-    public String getExtended() {
-        return extended;
-    }
+	public String getExtended() {
+		return extended;
+	}
 
-    public void setExtended(String extended) {
-        this.extended = extended;
-    }
+	public void setExtended(String extended) {
+		this.extended = extended;
+	}
 
-    public String getTemplate() {
-        return template;
-    }
+	public String getTemplate() {
+		return template;
+	}
 
-    public void setTemplate(String template) {
-        this.template = template;
-    }
+	public void setTemplate(String template) {
+		this.template = template;
+	}
 
-    public Map<String, AttributeReference> getVariables() {
-        return variables;
-    }
+	public Map<String, AttributeReference> getVariables() {
+		return variables;
+	}
 
-    public void setVariables(Map<String, AttributeReference> variables) {
-        this.variables = variables;
-    }
+	public void setVariables(Map<String, AttributeReference> variables) {
+		this.variables = variables;
+	}
+
+	public String getTrigger() {
+		return trigger;
+	}
+
+	public void setTrigger(String trigger) {
+		this.trigger = trigger;
+	}
+
+	public String getSection() {
+		return section;
+	}
+
+	public void setSection(String section) {
+		this.section = section;
+	}
+
+	public String getDocumentedId() {
+		return documentedId;
+	}
+
+	public void setDocumentedId(String documentedId) {
+		this.documentedId = documentedId;
+	}
 
 }
