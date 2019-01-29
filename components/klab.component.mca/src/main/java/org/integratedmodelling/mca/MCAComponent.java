3dd30543725a77f6a13932bb310cf8b9759bec43
fvilla <ferdinando.villa@gmail.com>
2019-01-29 10:38:43 +0100

    MCA semantic helper

--- components/klab.component.mca/src/main/java/org/integratedmodelling/mca/MCAComponent.java
+++ components/klab.component.mca/src/main/java/org/integratedmodelling/mca/MCAComponent.java
@@ -3,20 +3,41 @@ package org.integratedmodelling.mca;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.integratedmodelling.kim.api.UnarySemanticOperator;
 import org.integratedmodelling.klab.Version;
 import org.integratedmodelling.klab.api.extensions.Component;
+import org.integratedmodelling.klab.api.knowledge.IObservable;
+import org.integratedmodelling.klab.owl.Observable;
 
-@Component(id="org.integratedmodelling.mca", version=Version.CURRENT)
+@Component(id = "org.integratedmodelling.mca", version = Version.CURRENT)
 public class MCAComponent {
 
-    public static Set<String> criterionAnnotations;
-    static {
-    	criterionAnnotations = new HashSet<>();
-    	criterionAnnotations.add("cost");
-    	criterionAnnotations.add("benefit");
-    }
+	public static Set<String> criterionAnnotations;
+	static {
+		criterionAnnotations = new HashSet<>();
+		criterionAnnotations.add("cost");
+		criterionAnnotations.add("benefit");
+	}
 
 	public MCAComponent() {
-        // TODO Auto-generated constructor stub
-    }
+	}
+
+	/**
+	 * Build the value concept for a criterion that will be looked for in each
+	 * stakeholder to rank the criterion's importance. It will be a "value of
+	 * <i>criterion</i> for assessment of <i>finalTarget</i>".
+	 * 
+	 * @param criterionObservable
+	 * @param targetObservable
+	 * @return the finished value concept
+	 */
+	public static IObservable getCriterionValueObservable(IObservable criterionObservable,
+			IObservable targetObservable) {
+
+		return ((Observable) criterionObservable).getBuilder().as(UnarySemanticOperator.VALUE).withGoal(
+				((Observable) targetObservable).getBuilder().as(UnarySemanticOperator.ASSESSMENT).buildConcept())
+				.buildObservable();
+
+	}
+
 }
