c466424bcd5d19f6c86b23c6a9c9b285e9d0fef4
fvilla <ferdinando.villa@gmail.com>
2019-02-01 16:13:51 +0100

    Move on with MCA

--- components/klab.component.mca/src/main/java/org/integratedmodelling/mca/MCAComponent.java
+++ components/klab.component.mca/src/main/java/org/integratedmodelling/mca/MCAComponent.java
@@ -1,22 +1,36 @@
 package org.integratedmodelling.mca;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.integratedmodelling.kim.api.UnarySemanticOperator;
 import org.integratedmodelling.klab.Version;
 import org.integratedmodelling.klab.api.extensions.Component;
 import org.integratedmodelling.klab.api.knowledge.IObservable;
+import org.integratedmodelling.klab.api.model.IAnnotation;
+import org.integratedmodelling.klab.api.provenance.IArtifact;
+import org.integratedmodelling.klab.api.runtime.IComputationContext;
 import org.integratedmodelling.klab.owl.Observable;
+import org.integratedmodelling.mca.api.IAlternative;
+import org.integratedmodelling.mca.api.ICriterion;
+import org.integratedmodelling.mca.api.IStakeholder;
 
 @Component(id = "org.integratedmodelling.mca", version = Version.CURRENT)
 public class MCAComponent {
 
 	public static Set<String> criterionAnnotations;
+
 	static {
 		criterionAnnotations = new HashSet<>();
 		criterionAnnotations.add("cost");
 		criterionAnnotations.add("benefit");
+	}
+
+	public static enum Method {
+		EVAMIX, ELECTRE_I, ELECTRE_Is, ELECTRE_Iv, ELECTRE_II, ELECTRE_III, ELECTRE_IV, ELECTRE_TRI, TOPSIS
 	}
 
 	public MCAComponent() {
@@ -38,6 +52,79 @@ public class MCAComponent {
 				((Observable) targetObservable).getBuilder().as(UnarySemanticOperator.ASSESSMENT).buildConcept())
 				.buildObservable();
 
+	}
+
+	public static List<IAlternative> rank(List<IAlternative> alternatives, Collection<ICriterion> criteria,
+			IStakeholder observer, Method method) {
+
+		// TODO
+
+		return alternatives;
+	}
+
+	/**
+	 * Extract the elements of a MCA assessment from the passed artifact and put
+	 * them in the passed collections. If something goes wrong or the available data
+	 * do not support a MCA, warn through the context monitor and return false.
+	 * 
+	 * @param target
+	 * @param alternatives
+	 * @param criteria
+	 * @param observers
+	 * @param method
+	 * @param context
+	 * @return
+	 */
+	public static boolean extractAssessment(IArtifact target, List<IAlternative> alternatives,
+			Collection<ICriterion> criteria, List<IStakeholder> observers, Method method, IComputationContext context) {
+		// TODO Auto-generated method stub
+		
+		// check out the model for indication of the likely way to go
+		boolean outputsHaveCriteria = false;
+		boolean dependenciesHaveCriteria = false;
+		boolean alternativesHaveCriteria = false;
+		
+		List<IAnnotation> criterionAnnotations = new ArrayList<>();
+
+		for (int i = 1; i < context.getModel().getObservables().size(); i++) {
+			if (getCriterionAnnotation(context.getModel().getObservables().get(i)) != null) {
+				outputsHaveCriteria = true;
+				criterionAnnotations.add(getCriterionAnnotation(context.getModel().getObservables().get(i)));
+			}
+		}
+
+		for (IObservable observable :context.getModel().getDependencies()) {
+			if (getCriterionAnnotation(observable) != null) {
+				dependenciesHaveCriteria = true;
+				// can have both but must be different observables
+				criterionAnnotations.add(getCriterionAnnotation(observable));
+			}
+		}
+		
+		if (!outputsHaveCriteria && !dependenciesHaveCriteria) {
+			// must observe those on the alternatives
+		}
+		
+		// scan criteria: lookup in alternatives first
+		
+		// if alternatives have no states with annotations, alternatives may be in the model's dependencies and 
+		// have to be extracted from context states at the alternatives' locations
+		
+		return false;
+	}
+
+	class CriterionDescriptor {
+		IObservable observable;
+		boolean input;
+	}
+	
+	private static IAnnotation getCriterionAnnotation(IObservable observable) {
+		for (IAnnotation annotation : ((Observable)observable).getAnnotations()) {
+			if (criterionAnnotations.contains(annotation.getName())) {
+				return annotation;
+			}
+		}
+		return null;
 	}
 
 }
--- components/klab.component.mca/src/main/java/org/integratedmodelling/mca/services/RankingInstantiator.java
+++ components/klab.component.mca/src/main/java/org/integratedmodelling/mca/services/RankingInstantiator.java
@@ -1,5 +1,6 @@
 package org.integratedmodelling.mca.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.integratedmodelling.kim.api.IParameters;
@@ -8,33 +9,76 @@ import org.integratedmodelling.klab.api.data.artifacts.IObjectArtifact;
 import org.integratedmodelling.klab.api.data.general.IExpression;
 import org.integratedmodelling.klab.api.knowledge.IObservable;
 import org.integratedmodelling.klab.api.model.contextualization.IInstantiator;
+import org.integratedmodelling.klab.api.provenance.IArtifact;
 import org.integratedmodelling.klab.api.provenance.IArtifact.Type;
 import org.integratedmodelling.klab.api.runtime.IComputationContext;
+import org.integratedmodelling.klab.common.Geometry;
+import org.integratedmodelling.klab.components.runtime.observations.ObservationGroup;
 import org.integratedmodelling.klab.exceptions.KlabException;
+import org.integratedmodelling.mca.MCAComponent;
+import org.integratedmodelling.mca.MCAComponent.Method;
+import org.integratedmodelling.mca.api.IAlternative;
+import org.integratedmodelling.mca.api.ICriterion;
+import org.integratedmodelling.mca.api.IStakeholder;
 
 public class RankingInstantiator implements IInstantiator, IExpression {
 
+	String targetArtifact = null;
+	Method method = Method.ELECTRE_III;
+
 	@Override
 	public IGeometry getGeometry() {
-		// TODO Auto-generated method stub
-		return null;
+		return Geometry.create("#");
 	}
 
 	@Override
 	public Type getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return Type.OBJECT;
 	}
 
 	@Override
 	public Object eval(IParameters<String> parameters, IComputationContext context) throws KlabException {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO parameters
+		return new RankingInstantiator();
 	}
 
 	@Override
 	public List<IObjectArtifact> instantiate(IObservable semantics, IComputationContext context) throws KlabException {
-		// TODO Auto-generated method stub
+
+		IArtifact target = null;
+		if (targetArtifact == null) {
+			target = context.getTargetArtifact();
+		} else {
+			target = context.getArtifact(targetArtifact);
+		}
+
+		// must be a group
+		if (!(target instanceof ObservationGroup)) {
+			context.getMonitor()
+					.warn("MCA ranking cannot be performed on an artifact that is not a group of objects. Exiting.");
+			return null;
+		}
+
+		// Set up the MCA assessment
+		List<IAlternative> alternatives = new ArrayList<>();
+		List<ICriterion> criteria = new ArrayList<>();
+		List<IStakeholder> observers = new ArrayList<>();
+
+		// find criteria and possibly stakeholders
+		if (!MCAComponent.extractAssessment(target, alternatives, criteria, observers, method, context)) {
+			context.getMonitor().warn(
+					"MCA ranking cannot be performed due to inconsistent alternatives, criteria or stakeholders collected from context. Exiting.");
+			return null;
+		}
+
+		// run MCA
+		for (IStakeholder observer : observers) {
+			List<IAlternative> ranked = MCAComponent.rank(alternatives, criteria, observer, method);
+			// build and insert comparator in artifact
+		}
+
+
+		// we have no output of our own, so return null
 		return null;
 	}
 
