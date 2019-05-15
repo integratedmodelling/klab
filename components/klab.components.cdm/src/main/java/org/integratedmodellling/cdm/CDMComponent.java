5fcac9216924899599b84d1a0dfc694741b212b5
fvilla <ferdinando.villa@gmail.com>
2019-05-15 09:49:47 +0200

    Add stub files for CDM (NetCDF, OpenDAP) component

---
+++ components/klab.components.cdm/src/main/java/org/integratedmodellling/cdm/CDMComponent.java
@@ -0,0 +1,9 @@
+package org.integratedmodellling.cdm;
+
+import org.integratedmodelling.klab.Version;
+import org.integratedmodelling.klab.api.extensions.Component;
+
+@Component(id="org.integratedmodelling.cdm", version=Version.CURRENT)
+public class CDMComponent {
+
+}
---
+++ components/klab.components.cdm/src/main/java/org/integratedmodellling/cdm/adapter/CDMAdapter.java
@@ -0,0 +1,50 @@
+package org.integratedmodellling.cdm.adapter;
+
+import org.integratedmodelling.kim.api.IPrototype;
+import org.integratedmodelling.klab.Dataflows;
+import org.integratedmodelling.klab.Version;
+import org.integratedmodelling.klab.api.data.adapters.IResourceAdapter;
+import org.integratedmodelling.klab.api.data.adapters.IResourceEncoder;
+import org.integratedmodelling.klab.api.data.adapters.IResourceImporter;
+import org.integratedmodelling.klab.api.data.adapters.IResourcePublisher;
+import org.integratedmodelling.klab.api.data.adapters.IResourceValidator;
+import org.integratedmodelling.klab.api.extensions.ResourceAdapter;
+import org.integratedmodelling.klab.kim.Prototype;
+
+@ResourceAdapter(type = "cdm", version = Version.CURRENT)
+public class CDMAdapter implements IResourceAdapter {
+
+	@Override
+	public String getName() {
+		return "cdm";
+	}
+
+	@Override
+	public IResourceValidator getValidator() {
+		return new CDMValidator();
+	}
+
+	@Override
+	public IResourcePublisher getPublisher() {
+		return new CDMPublisher();
+	}
+
+	@Override
+	public IResourceEncoder getEncoder() {
+		return new CDMEncoder();
+	}
+
+	@Override
+	public IResourceImporter getImporter() {
+		return new CDMImporter();
+	}
+
+	@Override
+	public IPrototype getResourceConfiguration() {
+		return new Prototype(Dataflows.INSTANCE
+				.declare(getClass().getClassLoader()
+						.getResource("components/org.integratedmodelling.cdm/adapter/cdm.kdl"))
+				.getActuators().iterator().next(), null);
+	}
+
+}
