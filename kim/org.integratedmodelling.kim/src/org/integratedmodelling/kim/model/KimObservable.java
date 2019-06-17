e9a6cb8022240a1ea5b6b47ff8ba65049b7f33aa
Integrated Modelling Partnership <ferdinando.villa@bc3research.org>
2019-06-17 15:23:11 +0200

    Re-oops

--- kim/org.integratedmodelling.kim/src/org/integratedmodelling/kim/model/KimObservable.java
+++ kim/org.integratedmodelling.kim/src/org/integratedmodelling/kim/model/KimObservable.java
@@ -148,9 +148,9 @@ public class KimObservable extends KimStatement implements IKimObservable {
 			ret.downTo = KimConcept.normalize(declaration.getDownTo(), parent);
 		}
 
-		if (ret.formalName == null) {
-			ret.formalName = ret.getCodeName();
-		}
+//		if (ret.formalName == null) {
+//			ret.formalName = ret.getCodeName();
+//		}
 		
 		return ret;
 	}
