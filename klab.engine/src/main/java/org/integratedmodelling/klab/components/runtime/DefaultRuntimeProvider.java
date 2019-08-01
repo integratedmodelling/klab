f07ddf36e01a5d642c8e1024c4e36afbcca34ca8
Integrated Modelling Partnership <ferdinando.villa@bc3research.org>
2019-08-01 17:04:26 +0200

    Notes

--- klab.engine/src/main/java/org/integratedmodelling/klab/components/runtime/DefaultRuntimeProvider.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/components/runtime/DefaultRuntimeProvider.java
@@ -299,7 +299,7 @@ public class DefaultRuntimeProvider implements IRuntimeProvider {
 	public static IObservation createObservation(IObservable observable, IScale scale, RuntimeContext context,
 			boolean scalarStorage) {
 
-		boolean createActors = scale.getTime() != null;
+		boolean createActors = observable.is(Type.COUNTABLE) && scale.getTime().size() > 1;
 		Activity activity = null;
 
 		IIdentity identity = context.getMonitor().getIdentity();
