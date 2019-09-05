82a95fe07dd95395fd7fe86a865d2fe3758432a4
Integrated Modelling Partnership <ferdinando.villa@bc3research.org>
2019-09-05 17:30:06 +0200

    More options in storage definition

---
+++ klab.engine/src/main/java/org/integratedmodelling/klab/components/localstorage/debug/MemoryBackedAdaptiveStorage.java
@@ -0,0 +1,80 @@
+package org.integratedmodelling.klab.components.localstorage.debug;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.integratedmodelling.klab.api.data.IGeometry;
+import org.integratedmodelling.klab.api.provenance.IArtifact.Type;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import org.integratedmodelling.klab.components.localstorage.impl.AbstractAdaptiveStorage;
+
+public class MemoryBackedAdaptiveStorage<T> extends AbstractAdaptiveStorage<T> {
+
+	Map<Long, T[]> data = new HashMap<>();
+	Class<?> cls;
+
+	public MemoryBackedAdaptiveStorage(IGeometry geometry, Class<?> cls) {
+		super(geometry);
+		this.cls = cls;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void createBackendStorage(long timestep, T initialValue) {
+		if (data.get(timestep) == null) {
+			T[] slice =  (T[]) Array.newInstance(cls, (int)getSliceSize());
+			for (long n = 0; n < getSliceSize(); n++) {
+				slice[(int)n] = initialValue;
+			}
+			data.put(timestep, slice);
+		}
+	}
+
+	@Override
+	protected T getValueFromBackend(long offsetInSlice, long backendTimeSlice) {
+		return data.get(backendTimeSlice)[(int) offsetInSlice];
+	}
+
+	@Override
+	protected void setValueIntoBackend(T value, long offsetInSlice, long backendTimeSlice) {
+		data.get(backendTimeSlice)[(int) offsetInSlice] = value;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void duplicateBackendSlice(long sliceToCopy, long newSliceIndex) {
+		T[] slice = data.get(newSliceIndex);
+		T[] sourc = data.get(sliceToCopy);
+		if (slice == null) {
+			slice =  (T[]) Array.newInstance(cls, (int)getSliceSize());
+			data.put(newSliceIndex, slice);
+		}
+		for (int i = 0; i < slice.length; i++) {
+			slice[i] = sourc[i];
+		}
+		
+	}
+
+	public int getSliceCount() {
+		return super.sliceCount();
+	}
+
+	public int getBackendSliceCount() {
+		return data.size();
+	}
+
+	@Override
+	public Type getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
