facaee35f49a945b3e04eb23f9bfeadb8831a308
Ferdinando Villa <ferdinando.villa@gmail.com>
2020-01-31 16:03:01 +0100

    Comment out lazy loading of projects when a load request comes

--- klab.engine/src/main/java/org/integratedmodelling/klab/kim/KimNotifier.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/kim/KimNotifier.java
@@ -184,6 +184,8 @@ public class KimNotifier implements Kim.Notifier {
 	@Override
 	public INamespace synchronizeNamespaceWithRuntime(IKimNamespace namespace) {
 
+		System.out.println("POPPAMADONNA " + namespace.getName());
+		
 		Namespaces.INSTANCE.release(namespace.getName(), monitor);
 		Namespace ns = new Namespace(namespace);
 		Namespaces.INSTANCE.registerNamespace(ns, monitor);
