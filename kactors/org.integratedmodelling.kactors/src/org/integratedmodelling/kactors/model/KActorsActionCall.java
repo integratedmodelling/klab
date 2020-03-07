59924ef4d48a00e7853e14e6a2fa2db4ff0125d5
Ferdinando Villa <ferdinando.villa@bc3research.org>
2020-03-07 19:18:34 +0100

    Small changes

--- kactors/org.integratedmodelling.kactors/src/org/integratedmodelling/kactors/model/KActorsActionCall.java
+++ kactors/org.integratedmodelling.kactors/src/org/integratedmodelling/kactors/model/KActorsActionCall.java
@@ -4,8 +4,19 @@ import org.integratedmodelling.kactors.kactors.MessageCall;
 
 public class KActorsActionCall extends KActorsStatement {
 	
+	private String message;
+
 	public KActorsActionCall(MessageCall messageCall, KActorCodeStatement parent) {
 		super(messageCall, parent, Type.ACTION_CALL);
+		
+		this.message = messageCall.getName();
+		if (messageCall.getActions() != null) {
+//			for (messageCall.getActions().get
+		}
+	}
+
+	public String getMessage() {
+		return message;
 	}
 
 }
