d9ffb7fd3863085f020f334f3ee429621fa66b15
Ferdinando Villa <ferdinando.villa@gmail.com>
2020-12-18 14:02:17 +0100

    Test code for SDMX

--- components/klab.component.tables/src/main/java/org/integratedmodelling/tables/adapter/sdmx/SDMXInterpreter.java
+++ components/klab.component.tables/src/main/java/org/integratedmodelling/tables/adapter/sdmx/SDMXInterpreter.java
@@ -1,5 +1,6 @@
 package org.integratedmodelling.tables.adapter.sdmx;
 
+import java.util.List;
 import java.util.Map;
 
 import org.integratedmodelling.klab.api.data.IGeometry;
@@ -9,11 +10,21 @@ import org.integratedmodelling.klab.api.data.general.ITable.Structure;
 import org.integratedmodelling.klab.api.provenance.IArtifact.Type;
 import org.integratedmodelling.klab.api.runtime.IContextualizationScope;
 import org.integratedmodelling.klab.utils.Pair;
-import org.integratedmodelling.tables.ITableInterpreter;
 import org.integratedmodelling.tables.TableInterpreter;
 
+import it.bancaditalia.oss.sdmx.api.Dataflow;
+import it.bancaditalia.oss.sdmx.api.Dimension;
+import it.bancaditalia.oss.sdmx.api.PortableTimeSeries;
+import it.bancaditalia.oss.sdmx.client.SdmxClientHandler;
+
+/*
+ * Attributes: sdmx.dataflow, sdmx.provider, sdmx.query
+ * If provider.needsCredentials(): sdmx.user, sdmx.password
+ */
 public class SDMXInterpreter extends TableInterpreter {
 
+	
+	
 	@Override
 	public Type getType(IResource resource, IGeometry geometry) {
 		// TODO Auto-generated method stub
@@ -30,7 +41,24 @@ public class SDMXInterpreter extends TableInterpreter {
 	public void encode(IResource resource, Map<String, String> urnParameters, IGeometry geometry, Builder builder,
 			IContextualizationScope context) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		String provider = "EUROSTAT";
+		// this returns about 7000 dataflow descriptors
+		final Map<String, Dataflow> flows = SdmxClientHandler.getFlowObjects(provider, null);
+		// choose a flow ID among the results. Check out GGH emissions at sdg_13_10
+		String dataflowID = "sdg_15_50";
+		// each dimension has name, offset and codelist
+		List<Dimension> dimensions = SdmxClientHandler.getDimensions(provider, dataflowID);
+		// Build timeseries query key using the dimensions and the codelists
+		String tsQuery = "sdg_15_50/A.SEV.CLC2_3X331_332_335.KM2+PC.BE+BG+CH+AT+CY";
+		List<PortableTimeSeries<Double>> result = SdmxClientHandler.getTimeSeriesNames(provider, tsQuery);
+		// key method seems to be getTimeseries(provider, query, String startTime,
+		// String endTime, boolean serieskeysonly, String updatedAfter, boolean includeHistory) - previous calls it
+		// with default parameters, i.e. no data, just names
+		// Check interface docs in GenericSDMXClient
 	}
 
 }
