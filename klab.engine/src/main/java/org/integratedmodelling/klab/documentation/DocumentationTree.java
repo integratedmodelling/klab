5dcc7b7c6566a74af06f7abaf08359ae8c4690e4
Ferdinando Villa <ferdinando.villa@gmail.com>
2021-03-10 17:42:04 +0100

    Stubs for later

---
+++ klab.engine/src/main/java/org/integratedmodelling/klab/documentation/DocumentationTree.java
@@ -0,0 +1,13 @@
+package org.integratedmodelling.klab.documentation;
+
+/**
+ * The structured version of the report, to substitute the simpler report based on a document view.
+ * Uses the beans from the rest package directly. Each context holds a doctree with info on
+ * everything that has gone through it. The tree is serializable to JSON.
+ * 
+ * @author Ferd
+ *
+ */
+public class DocumentationTree {
+
+}
