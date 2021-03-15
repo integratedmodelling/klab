5a825cd31644fced1486baf3f1e797d1406369cd
Ferdinando Villa <ferdinando.villa@bc3research.org>
2021-03-15 08:47:59 +0100

    Preparing for structured documentation

--- klab.engine/src/main/java/org/integratedmodelling/klab/documentation/DocumentationTree.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/documentation/DocumentationTree.java
@@ -1,13 +1,35 @@
 package org.integratedmodelling.klab.documentation;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.integratedmodelling.klab.api.runtime.ISession;
+import org.integratedmodelling.klab.engine.runtime.api.IRuntimeScope;
+import org.integratedmodelling.klab.utils.Path;
+
 /**
  * The structured version of the report, to substitute the simpler report based on a document view.
- * Uses the beans from the rest package directly. Each context holds a doctree with info on
- * everything that has gone through it. The tree is serializable to JSON.
+ * Uses the beans from the rest package directly. Each report holds a doctree with info on
+ * everything that has gone through it. The tree is serializable to JSON. Eventually this may
+ * substitute the Report object in its entirety.
  * 
  * @author Ferd
  *
  */
 public class DocumentationTree {
 
+    Map<String, DocumentationItem> items = new HashMap<>();
+    
+    public DocumentationTree() {
+        // 
+    }
+    
+    public DocumentationTree(IRuntimeScope context, ISession identity) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void add(Object o) {
+        System.out.println("Got shitteron " + Path.getLast(o.getClass().getCanonicalName(), '.') + ": " + o);
+    }
+    
 }
