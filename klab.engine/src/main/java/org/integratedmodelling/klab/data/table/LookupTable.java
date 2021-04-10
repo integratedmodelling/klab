b213faca9b69d8d2d18dca03959ada55fb46b217
Ferdinando Villa <ferdinando.villa@bc3research.org>
2021-04-10 12:33:52 +0200

    Two-way tables working

--- klab.engine/src/main/java/org/integratedmodelling/klab/data/table/LookupTable.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/data/table/LookupTable.java
@@ -13,6 +13,7 @@ import org.integratedmodelling.kim.api.IKimClassifier;
 import org.integratedmodelling.kim.api.IKimConcept;
 import org.integratedmodelling.kim.api.IKimExpression;
 import org.integratedmodelling.kim.api.IKimLookupTable;
+import org.integratedmodelling.kim.api.IKimLookupTable.Argument.Dimension;
 import org.integratedmodelling.kim.api.IParameters;
 import org.integratedmodelling.klab.Concepts;
 import org.integratedmodelling.klab.api.data.classification.IClassifier;
@@ -27,243 +28,265 @@ import org.integratedmodelling.klab.utils.Pair;
 
 public class LookupTable implements ILookupTable {
 
-	class ArgImpl implements Argument {
+    class ArgImpl implements Argument {
 
-		String id;
-		IConcept concept;
+        String id;
+        IConcept concept;
+        public Dimension dimension;
 
-		@Override
-		public String getId() {
-			return id;
-		}
+        @Override
+        public String getId() {
+            return id;
+        }
 
-		@Override
-		public IConcept getConcept() {
-			return concept;
-		}
-	}
+        @Override
+        public IConcept getConcept() {
+            return concept;
+        }
+    }
 
-	Table<IClassifier> table;
-	List<Argument> variables = new ArrayList<>();
-	IArtifact.Type type;
-	int searchIndex;
+    Table<IClassifier> table;
+    List<Argument> variables = new ArrayList<>();
+    IArtifact.Type type;
+    int searchIndex;
 
-	Map<IConcept, Integer> key;
-	Map<String, Object> cache = new HashMap<>();
+    Map<IConcept, Integer> key;
+    Map<String, Object> cache = new HashMap<>();
     private boolean twoWay;
     List<IClassifier> columnClassifiers = new ArrayList<>();
     List<IClassifier> rowClassifiers = new ArrayList<>();
-    
 
-	class RowProxy {
-		int row;
+    class RowProxy {
+        int row;
 
-		RowProxy(int row) {
-			this.row = row;
-		}
-	}
+        RowProxy(int row) {
+            this.row = row;
+        }
+    }
 
-	public LookupTable(IKimLookupTable lookupTable) {
+    public LookupTable(IKimLookupTable lookupTable) {
 
-		this.table = Table.create(lookupTable.getTable());
-		this.twoWay = lookupTable.isTwoWay();
-		
-		for (IKimLookupTable.Argument a : lookupTable.getArguments()) {
-			ArgImpl aa = new ArgImpl();
-			if (a.id != null) {
-				aa.id = a.id;
-			} else if (a.concept != null) {
-				aa.concept = Concepts.INSTANCE.declare(a.concept);
-			}
-			this.variables.add(aa);
-		}
-		this.searchIndex = lookupTable.getLookupColumnIndex();
-		this.type = lookupTable.getLookupType();
+        this.table = Table.create(lookupTable.getTable());
+        this.twoWay = lookupTable.isTwoWay();
 
-		if (this.type == Type.CONCEPT) {
-			this.key = new LinkedHashMap<>();
-			for (int i = 0; i < table.getRowCount(); i++) {
-				this.key.put((IConcept) table.getRow(i)[searchIndex].asValue(null), i);
-			}
-		}
-		
-		if (this.twoWay) {
-		    for (IKimClassifier classifier : lookupTable.getColumnClassifiers()) {
-		        columnClassifiers.add(new Classifier(classifier));
-		    }
+        for (IKimLookupTable.Argument a : lookupTable.getArguments()) {
+            ArgImpl aa = new ArgImpl();
+            if (a.id != null) {
+                aa.id = a.id;
+            } else if (a.concept != null) {
+                aa.concept = Concepts.INSTANCE.declare(a.concept);
+            }
+            aa.dimension = a.dimension;
+            this.variables.add(aa);
+        }
+        this.searchIndex = lookupTable.getLookupColumnIndex();
+        this.type = lookupTable.getLookupType();
+
+        if (this.type == Type.CONCEPT) {
+            this.key = new LinkedHashMap<>();
+            for (int i = 0; i < table.getRowCount(); i++) {
+                this.key.put((IConcept) table.getRow(i)[searchIndex].asValue(null), i);
+            }
+        }
+
+        if (this.twoWay) {
+            for (IKimClassifier classifier : lookupTable.getColumnClassifiers()) {
+                columnClassifiers.add(new Classifier(classifier));
+            }
             for (IKimClassifier classifier : lookupTable.getRowClassifiers()) {
                 rowClassifiers.add(new Classifier(classifier));
             }
-		}
+        }
 
-	}
+    }
 
-	/**
-	 * True if the table is functional, i.e. it matches one input to one output.
-	 * 
-	 * @return
-	 */
-	public boolean isKey() {
-		return key != null;
-	}
+    /**
+     * True if the table is functional, i.e. it matches one input to one output.
+     * 
+     * @return
+     */
+    public boolean isKey() {
+        return key != null;
+    }
 
-	@Override
-	public IStructuredTable<IClassifier> getTable() {
-		return table;
-	}
+    @Override
+    public IStructuredTable<IClassifier> getTable() {
+        return table;
+    }
 
-	@Override
-	public int reverseLookup(Object value) {
-		Integer ret = key.get(value);
-		return ret == null ? -1 : ret;
-	}
+    @Override
+    public int reverseLookup(Object value) {
+        Integer ret = key.get(value);
+        return ret == null ? -1 : ret;
+    }
 
-	@Override
-	public int size() {
-		return table.getRowCount();
-	}
+    @Override
+    public int size() {
+        return table.getRowCount();
+    }
 
-	@Override
-	public int getResultColumn() {
-		return searchIndex;
-	}
+    @Override
+    public int getResultColumn() {
+        return searchIndex;
+    }
 
-	@Override
-	public List<String> getLabels() {
-		List<String> ret = new ArrayList<>();
-		for (IConcept concept : key.keySet()) {
-			ret.add(Concepts.INSTANCE.getDisplayName(concept));
-		}
-		return ret;
-	}
+    @Override
+    public List<String> getLabels() {
+        List<String> ret = new ArrayList<>();
+        for (IConcept concept : key.keySet()) {
+            ret.add(Concepts.INSTANCE.getDisplayName(concept));
+        }
+        return ret;
+    }
 
-	@Override
-	public boolean isOrdered() {
-		return key != null && ((IConcept) table.getRow(0)[searchIndex].asValue(null)).is(IKimConcept.Type.ORDERING);
-	}
+    @Override
+    public boolean isOrdered() {
+        return key != null && ((IConcept) table.getRow(0)[searchIndex].asValue(null)).is(IKimConcept.Type.ORDERING);
+    }
 
-	@Override
-	public List<Pair<Integer, String>> getAllValues() {
-		List<Pair<Integer, String>> ret = new ArrayList<>();
-		for (Entry<IConcept, Integer> entry : key.entrySet()) {
-			ret.add(new Pair<>(entry.getValue(), Concepts.INSTANCE.getDisplayName(entry.getKey())));
-		}
-		return ret;
-	}
+    @Override
+    public List<Pair<Integer, String>> getAllValues() {
+        List<Pair<Integer, String>> ret = new ArrayList<>();
+        for (Entry<IConcept, Integer> entry : key.entrySet()) {
+            ret.add(new Pair<>(entry.getValue(), Concepts.INSTANCE.getDisplayName(entry.getKey())));
+        }
+        return ret;
+    }
 
-	@Override
-	public List<Argument> getArguments() {
-		return variables;
-	}
+    @Override
+    public List<Argument> getArguments() {
+        return variables;
+    }
 
-	@Override
-	public Object lookup(IParameters<String> parameters, IContextualizationScope context) {
+    @Override
+    public Object lookup(IParameters<String> parameters, IContextualizationScope context) {
 
-	    if (this.twoWay) {
+        StringBuffer s = new StringBuffer(1024);
+        Object[] values = new Object[variables.size()];
+        int rowDimension = -1;
 
-	        /*
-	         * match the parameters to row and column, fail if match is not possible
-	         */
-	        
-	        /*
-	         * return the value @x,y
-	         */
-	        return null;
-	    }
-	    
-		StringBuffer s = new StringBuffer(1024);
-		Object[] values = new Object[variables.size()];
+        for (int i = 0; i < variables.size(); i++) {
+            if (i == searchIndex || (variables.get(i).getId() != null && variables.get(i).getId().charAt(0) == '*')) {
+                continue;
+            }
+            if (twoWay && ((ArgImpl)variables.get(i)).dimension == Dimension.ROW) {
+                rowDimension = i;
+            }
+            values[i] = variables.get(i).getId() != null
+                    ? parameters.get(variables.get(i).getId())
+                    : context.localizePredicate(variables.get(i).getConcept());
+            s.append("|");
+            s.append(values[i] == null ? "null" : values[i].toString());
+        }
+        String key = s.toString();
 
-		for (int i = 0; i < variables.size(); i++) {
-			if (i == searchIndex || (variables.get(i).getId() != null && variables.get(i).getId().charAt(0) == '*')) {
-				continue;
-			}
-			values[i] = variables.get(i).getId() != null ? parameters.get(variables.get(i).getId())
-					: context.localizePredicate(variables.get(i).getConcept());
-			s.append("|");
-			s.append(values[i] == null ? "null" : values[i].toString());
-		}
-		String key = s.toString();
+        Object ret = cache.get(key);
+        if (ret == null) {
 
-		Object ret = cache.get(key);
-		if (ret == null) {
+            int rind = 0;
+            boolean storeProxy = false;
+            boolean doNotCache = false;
 
-			int rind = 0;
-			boolean storeProxy = false;
-			for (IClassifier[] row : table.getRows()) {
-				boolean ok = true;
-				for (int i = 0; i < variables.size(); i++) {
-					if (i == searchIndex
-							|| (variables.get(i).getId() != null && variables.get(i).getId().charAt(0) == '*')) {
-						continue;
-					}
-					if (!row[i].classify(values[i], context)) {
-						ok = false;
-						break;
-					}
-				}
-				if (ok) {
-					if (row[searchIndex].isComputed()) {
-						storeProxy = true;
-					}
-					ret = row[searchIndex].asValue(context);
-					break;
-				}
-				rind++;
-			}
+            if (this.twoWay && rowDimension >= 0) {
 
-			cache.put(key, ret == null ? Optional.empty() : (storeProxy ? new RowProxy(rind) : ret));
+                /*
+                 * match the parameters to row and column, fail if match is not possible
+                 * 
+                 */
+                Object value = values[rowDimension];
+                for (int rowIndex = 0; rowIndex < rowClassifiers.size(); rowIndex++) {
+                    if (rowClassifiers.get(rowIndex).classify(value, context)) {
+                        for (int colIndex = 0; colIndex < columnClassifiers.size(); colIndex++) {
+                            if (columnClassifiers.get(colIndex).classify(values[rowDimension == 0 ? 1 : 0], context)) {
+                                if (table.getRow(rowIndex)[colIndex].isComputed()) {
+                                    doNotCache = true;
+                                }
+                                ret = table.getRow(rowIndex)[colIndex].asValue(context);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                
+            } else {
 
-		} else if (ret instanceof RowProxy) {
-			ret = table.getRows().get(((RowProxy) ret).row)[searchIndex].asValue(context);
-		} else {
-			ret = ret instanceof Optional ? null : ret;
-		}
+                for (IClassifier[] row : table.getRows()) {
+                    boolean ok = true;
+                    for (int i = 0; i < variables.size(); i++) {
+                        if (i == searchIndex || (variables.get(i).getId() != null && variables.get(i).getId().charAt(0) == '*')) {
+                            continue;
+                        }
+                        if (!row[i].classify(values[i], context)) {
+                            ok = false;
+                            break;
+                        }
+                    }
+                    if (ok) {
+                        if (row[searchIndex].isComputed()) {
+                            storeProxy = true;
+                        }
+                        ret = row[searchIndex].asValue(context);
+                        break;
+                    }
+                    rind++;
+                }
+            }
 
-		return ret;
-	}
+            if (!doNotCache) {
+                cache.put(key, ret == null ? Optional.empty() : (storeProxy ? new RowProxy(rind) : ret));
+            }
+            
+        } else if (ret instanceof RowProxy) {
+            ret = table.getRows().get(((RowProxy) ret).row)[searchIndex].asValue(context);
+        } else {
+            ret = ret instanceof Optional ? null : ret;
+        }
 
-	@Override
-	public IArtifact.Type getResultType() {
-		return type;
-	}
+        return ret;
+    }
 
-	@Override
-	public Object lookup(int index) {
-		return (IConcept) table.getRow(index)[searchIndex].asValue(null);
-	}
+    @Override
+    public IArtifact.Type getResultType() {
+        return type;
+    }
 
-	public Collection<IKimExpression> getUniqueExpressions() {
-		return table.getExpressions();
-	}
+    @Override
+    public Object lookup(int index) {
+        return (IConcept) table.getRow(index)[searchIndex].asValue(null);
+    }
 
-	@Override
-	public List<String> getSerializedObjects() {
-		List<String> ret = new ArrayList<>();
-		for (IConcept concept : key.keySet()) {
-			ret.add(concept.getDefinition());
-		}
-		return ret;
-	}
+    public Collection<IKimExpression> getUniqueExpressions() {
+        return table.getExpressions();
+    }
 
-	@Override
-	public List<IConcept> getConcepts() {
-		List<IConcept> ret = new ArrayList<>();
-		for (IConcept concept : key.keySet()) {
-			ret.add(concept);
-		}
-		return ret;
-	}
+    @Override
+    public List<String> getSerializedObjects() {
+        List<String> ret = new ArrayList<>();
+        for (IConcept concept : key.keySet()) {
+            ret.add(concept.getDefinition());
+        }
+        return ret;
+    }
 
-	@Override
-	public void include(Object value) {
-		if (!(value instanceof IConcept)) {
-			throw new IllegalArgumentException("a table can only serve as a datakey for concepts");
-		}
-		if (!this.key.containsKey((IConcept) value)) {
-			this.key.put((IConcept) value, this.key.size());
-		}
-	}
+    @Override
+    public List<IConcept> getConcepts() {
+        List<IConcept> ret = new ArrayList<>();
+        for (IConcept concept : key.keySet()) {
+            ret.add(concept);
+        }
+        return ret;
+    }
+
+    @Override
+    public void include(Object value) {
+        if (!(value instanceof IConcept)) {
+            throw new IllegalArgumentException("a table can only serve as a datakey for concepts");
+        }
+        if (!this.key.containsKey((IConcept) value)) {
+            this.key.put((IConcept) value, this.key.size());
+        }
+    }
 
     public boolean isTwoWay() {
         return twoWay;
