d6c5f1d4ca37b935a1f2efdb972d84f80dd983df
steve <steven.wohl@bc3research.org>
2021-04-23 12:11:30 +0200

    Reset the session activity with touch when obs scale history trigger the session listener.  Add auto wired value for session deadband.

--- products/cloud/src/main/java/org/integratedmodelling/klab/engine/services/EngineReadyService.java
+++ products/cloud/src/main/java/org/integratedmodelling/klab/engine/services/EngineReadyService.java
@@ -5,6 +5,7 @@ import org.integratedmodelling.klab.Logging;
 import org.integratedmodelling.klab.engine.RemoteEngineService;
 import org.integratedmodelling.klab.engine.events.UserEventPublisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
@@ -21,13 +22,16 @@ public class EngineReadyService {
 	@Autowired
 	UserEventPublisher publisher;
 	
+	@Value("${engine.sessions.inactive:60}")
+    Long inactive;
+	
     @EventListener(ApplicationReadyEvent.class)
     public void ContextRefreshedEventExecute(){
         engineService.getEngine().setPublisher(publisher);
+        engineService.getEngine().setSessionDeadBand(inactive);
         Logging.INSTANCE.info("Started Agent Check Port");
         check.start();
         engineService.getEngine().setCheck(check);
-
     }
     
 }
