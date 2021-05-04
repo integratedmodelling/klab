c33eaeac1828a3856db89483bcc87ab0b8184c3e
Andrea Antonello <andrea.antonello@gmail.com>
2021-05-04 15:29:59 +0200

    try to get output of process

--- components/klab.component.hydrology/src/main/java/org/integratedmodelling/geoprocessing/hydrology/BaseFlowWaterVolumeResolver.java
+++ components/klab.component.hydrology/src/main/java/org/integratedmodelling/geoprocessing/hydrology/BaseFlowWaterVolumeResolver.java
@@ -16,6 +16,7 @@ import org.integratedmodelling.klab.api.model.IModel;
 import org.integratedmodelling.klab.api.model.contextualization.IResolver;
 import org.integratedmodelling.klab.api.observations.IProcess;
 import org.integratedmodelling.klab.api.observations.IState;
+import org.integratedmodelling.klab.api.observations.scale.space.IGrid.Cell;
 import org.integratedmodelling.klab.api.observations.scale.space.ISpace;
 import org.integratedmodelling.klab.api.provenance.IArtifact.Type;
 import org.integratedmodelling.klab.api.runtime.IContextualizationScope;
@@ -37,40 +38,36 @@ public class BaseFlowWaterVolumeResolver implements IResolver<IProcess>, IExpres
     @Override
     public IProcess resolve(IProcess baseflowProcess, IContextualizationScope context) throws KlabException {
 
-        IState baseflowVolumeState = context.getArtifact("base_flow_water_volume", IState.class);
+        IState baseflowVolumeState = getBaseFlowState(context);
+
         IState infiltratedWaterVolumeState = context.getArtifact("infiltrated_water_volume", IState.class);
 
-        Grid baseflowGrid = Space.extractGrid(baseflowVolumeState);
-        if (baseflowGrid == null) {
-            throw new KlabValidationException("Baseflow must be computed on a grid extent");
-        }
+//        Grid baseflowGrid = Space.extractGrid(baseflowVolumeState);
+//        if (baseflowGrid == null) {
+//            throw new KlabValidationException("Baseflow must be computed on a grid extent");
+//        }
         for(ILocator locator : context.getScale()) {
-//          locator.as(null) -> cell
+            Cell cell = locator.as(Cell.class);
 
-            for(int i = 0; i < baseflowGrid.getCellCount(); i++) {
-                long[] xy = baseflowGrid.getXYOffsets(i);
-                Double value = itera.getSampleDouble((int) xy[0], (int) xy[1], 0);
-                ILocator spl = locator.at(ISpace.class, xy[0], xy[1]);
-                if (transformation != null) {
-                    value = transformation.apply(value);
-                }
-                if (coordinateChecker != null) {
-                    if (!coordinateChecker.apply(xy)) {
-                        value = Double.NaN;
-                    }
-                }
-                for(ILocator spp : spl) {
-                    state.set(spp, value);
-                }
+            Double baseFlow = baseflowVolumeState.get(cell, Double.class);
+            if (baseFlow == null) {
+                baseFlow = 0.0;
+                baseflowVolumeState.set(cell, baseFlow);
+            } else {
+                Double infiltration = infiltratedWaterVolumeState.get(cell, Double.class);
+                double[] center = cell.getCenter();
+                baseFlow = center[0] + baseFlow * center[1]; // DUMMY test formula
+
+                infiltration = infiltration * baseFlow; // DUMMY test formula
             }
         }
 
-        GridCoverage2D infiltratedWaterVolumeGC = GeotoolsUtils.INSTANCE.stateToCoverage(infiltratedWaterVolumeState,
-                context.getScale(), DataBuffer.TYPE_FLOAT, floatNovalue, false);
-
-        double[] values = new double[1];
-        infiltratedWaterVolumeGC.evaluate(new GridCoordinates2D(0, 0), values);
-        context.getMonitor().info("Got infiltrated with value in 0,0:  " + values[0]);
+//        GridCoverage2D infiltratedWaterVolumeGC = GeotoolsUtils.INSTANCE.stateToCoverage(infiltratedWaterVolumeState,
+//                context.getScale(), DataBuffer.TYPE_FLOAT, floatNovalue, false);
+//
+//        double[] values = new double[1];
+//        infiltratedWaterVolumeGC.evaluate(new GridCoordinates2D(0, 0), values);
+//        context.getMonitor().info("Got infiltrated with value in 0,0:  " + values[0]);
 
 //        IState baseflowState = null;
 //
@@ -100,9 +97,30 @@ public class BaseFlowWaterVolumeResolver implements IResolver<IProcess>, IExpres
 //        }
 
         // DUMMY PLACEHOLDER OPERATION
-        context.getMonitor().info("Processing Baseflow the dummy way. Just a placeholder.");
+        context.getMonitor().info("Processing Baseflow with dummy formulae.");
 
         return baseflowProcess;
+    }
+
+    private IState getBaseFlowState(IContextualizationScope context) {
+        String localName = "base_flow_water_volume";
+        IState baseflowVolumeState = context.getArtifact(localName, IState.class);
+        if (baseflowVolumeState == null) {
+            IModel model = context.getModel();
+            List<IObservable> observables = model.getObservables();
+            for(int i = 1; i < observables.size(); i++) {
+                if (localName.equals(context.getModel().getObservables().get(i).getName())) {
+                    IState state = context.getArtifact(localName, IState.class);
+                    if (state == null) {
+                        context.getMonitor().warn("cannot find state for " + localName);
+                    } else {
+                        baseflowVolumeState = state;
+                    }
+                    break;
+                }
+            }
+        }
+        return baseflowVolumeState;
     }
 
     @Override
