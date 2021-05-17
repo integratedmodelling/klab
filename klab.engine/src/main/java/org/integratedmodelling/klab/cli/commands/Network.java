00e3c14039ec627fed0bdcd7f5517ebabd638b47
Ferdinando Villa <ferdinando.villa@gmail.com>
2021-05-17 15:42:50 +0200

    Add retrieval, update and display of uptime in nodes

--- klab.engine/src/main/java/org/integratedmodelling/klab/cli/commands/Network.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/cli/commands/Network.java
@@ -7,14 +7,16 @@ import org.integratedmodelling.klab.api.auth.INetworkSessionIdentity;
 import org.integratedmodelling.klab.api.auth.INodeIdentity;
 import org.integratedmodelling.klab.api.cli.ICommand;
 import org.integratedmodelling.klab.api.runtime.ISession;
+import org.joda.time.Duration;
+import org.joda.time.format.PeriodFormat;
 
 public class Network implements ICommand {
 
-	@Override
-	public Object execute(IServiceCall call, ISession session) {
+    @Override
+    public Object execute(IServiceCall call, ISession session) {
 
-		if (((List<?>) call.getParameters().get("arguments")).size() > 0) {
-			String arg = ((List<?>) call.getParameters().get("arguments")).get(0).toString();
+        if (((List<?>) call.getParameters().get("arguments")).size() > 0) {
+            String arg = ((List<?>) call.getParameters().get("arguments")).get(0).toString();
             /*
              * if ("on".equals(arg)) { if (!CliRuntime.INSTANCE .startNetwork(
              * call.getParameters().get("browser", false) ? () -> BrowserUtils
@@ -23,19 +25,21 @@ public class Network implements ICommand {
              * session.getMonitor().error("Please wait until engine is active"); } } else if
              * ("off".equals(arg)) { CliRuntime.INSTANCE.stopNetwork(); } else
              */if ("nodes".equals(arg)) {
-				INetworkSessionIdentity network = session.getParentIdentity(INetworkSessionIdentity.class);
-				String ret = "";
-				if (network != null) {
-					for (INodeIdentity node : network.getNodes()) {
-						ret += (ret.isEmpty() ? "" : "\n") + "   " + node.getName() + " at " + node.getUrls() + node.getAdapters().toString();
-					}
-				}
-				return ret;
-			} else {
-				session.getMonitor().error("Network services may only be turned on or off");
-			}
-		}
-		return null;
-	}
+                INetworkSessionIdentity network = session.getParentIdentity(INetworkSessionIdentity.class);
+                String ret = "";
+                if (network != null) {
+                    for (INodeIdentity node : network.getNodes()) {
+                        Duration uptime = new Duration(node.getUptime());
+                        ret += (ret.isEmpty() ? "" : "\n") + "   " + node.getName() + " at " + node.getUrls()
+                                + node.getAdapters().toString() + " (up " + PeriodFormat.getDefault().print(uptime.toPeriod()) + ")";
+                    }
+                }
+                return ret;
+            } else {
+                session.getMonitor().error("Network services may only be turned on or off");
+            }
+        }
+        return null;
+    }
 
 }
