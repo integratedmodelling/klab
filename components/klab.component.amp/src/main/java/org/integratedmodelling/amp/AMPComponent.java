edd72d38771e93ab63c8781be714650aede21365
Ferdinando Villa <ferdinando.villa@gmail.com>
2021-06-08 17:28:00 +0200

    Stub of the AMP component (now linked to nodes)

---
+++ components/klab.component.amp/src/main/java/org/integratedmodelling/amp/AMPComponent.java
@@ -0,0 +1,9 @@
+package org.integratedmodelling.amp;
+
+import org.integratedmodelling.klab.Version;
+import org.integratedmodelling.klab.api.extensions.Component;
+
+@Component(id = "org.integratedmodelling.amp", version = Version.CURRENT)
+public class AMPComponent {
+
+}
