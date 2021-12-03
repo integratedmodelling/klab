54e093e05a63d0a1cc85918d9217470992a59326
Steven Wohl <steven.wohl@bc3research.org>
2021-12-03 15:17:00 +0100

    Hotfix/remote engine self destruct (#5)
    * Remote engine aspect for catching out of disk space exeptions.
    
    * allow only 1 tag to be passed to jib
    
    * Aspect not working in project, moved to a scheduler executor for
    checking the list.  Likely could make this a flagged feature so it could
    be disabled/configured.
    
    * Add JenkinsFile and change poms to only use the env.TAG.  Added
    node.cert to resources.
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    webhook hell
    
    * Update Jenkinsfile
    
    do saved parameters work? or are defaults always winning?
    
    * remove aspect enable annotation
    
    * Update Jenkinsfile
    
    seems the variables are cleared once the job is run automatically...
    
    * Update Jenkinsfile
    
    lets trigger this again
    
    * Update Jenkinsfile
    
    seems like i need to leave them in..
    
    * Update Jenkinsfile
    
    now it trigger to test my theory

--- products/cloud/src/main/java/org/integratedmodelling/klab/engine/configs/SpringAsyncConfig.java
+++ products/cloud/src/main/java/org/integratedmodelling/klab/engine/configs/SpringAsyncConfig.java
@@ -5,19 +5,21 @@ import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
+@EnableScheduling
 public class SpringAsyncConfig implements AsyncConfigurer {
     
     @Override
     public Executor getAsyncExecutor() {
          ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
-         threadPool.setCorePoolSize(2);
-         threadPool.setMaxPoolSize(2);
+         threadPool.setCorePoolSize(4);
+         threadPool.setMaxPoolSize(50);
          threadPool.setQueueCapacity(500);
-         threadPool.setThreadNamePrefix("stat-server-post");
+         threadPool.setThreadNamePrefix("remote-thread-execute");
          threadPool.initialize();
          return threadPool;
     }
---
+++ products/cloud/src/main/java/org/integratedmodelling/klab/engine/services/RemoteEngineDiskWatcher.java
@@ -0,0 +1,29 @@
+package org.integratedmodelling.klab.engine.services;
+
+import java.io.File;
+
+import org.integratedmodelling.klab.Logging;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RemoteEngineDiskWatcher {
+	
+	@Autowired
+	AgentServiceCheck agent;
+	
+	@Scheduled(fixedDelay = 10000)
+	public void detectDiskFull() {
+		File tmpdir = new File(System.getProperty("java.io.tmpdir"));
+		double freeSpace = tmpdir.getFreeSpace() * 0.000001;
+		Logging.INSTANCE.info("Temporary Disk free space: " + freeSpace);
+		if (freeSpace < 10) {
+			System.exit(0);
+		}
+		if (freeSpace < 100) {
+			int halfWeight = agent.weight / 2 + 1;
+			agent.setweight(halfWeight);
+		}
+	}
+}
