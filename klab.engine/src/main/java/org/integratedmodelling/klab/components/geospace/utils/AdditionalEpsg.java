b78c1e1ad8e13d6b91bf913dd4203a2a45ee1234
Andrea Antonello <andrea.antonello@gmail.com>
2022-02-09 12:02:38 +0100

    fix for wrong epsg number

--- klab.engine/src/main/java/org/integratedmodelling/klab/components/geospace/utils/AdditionalEpsg.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/components/geospace/utils/AdditionalEpsg.java
@@ -19,7 +19,7 @@ import org.geotools.util.factory.Hints;
  * wkt representation. The latter needs to be formatted into a single line srting.   
  */
 public enum AdditionalEpsg {
-    EPSG9221(9211,
+    EPSG9221(9221,
             "PROJCS[\"Hartebeesthoek94 / ZAF BSU Albers 25E\",GEOGCS[\"Hartebeesthoek94\",DATUM[\"Hartebeesthoek94_based_on_WGS84_ellipsoid\",SPHEROID[\"WGS 84\",6378137,298.257223563,AUTHORITY[\"EPSG\",\"7030\"]],TOWGS84[0,0,0,0,0,0,0]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\"8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]]],PROJECTION[\"Albers_Conic_Equal_Area\"],PARAMETER[\"latitude_of_center\",-30],PARAMETER[\"longitude_of_center\",25],PARAMETER[\"standard_parallel_1\",-22],PARAMETER[\"standard_parallel_2\",-38],PARAMETER[\"false_easting\",1400000],PARAMETER[\"false_northing\",1300000],UNIT[\"metre\",1,AUTHORITY[\"EPSG\",\"9001\"]],AXIS[\"Easting\",EAST],AXIS[\"Northing\",NORTH]]");
 
     private String projWkt;
@@ -41,7 +41,7 @@ public enum AdditionalEpsg {
      */
     public static void registerAdditionalEpsgCodes() throws Exception {
 
-        File file = File.createTempFile("epsg", "properties");
+        File file = File.createTempFile("epsg", ".properties");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));) {
             for(AdditionalEpsg epsg : AdditionalEpsg.values()) {
                 writer.append(epsg.getProperty());
