3e274f27d486138a6f4f2fe91ab48f77fa8ae195
Ferdinando Villa <ferdinando.villa@gmail.com>
2022-09-26 15:18:20 +0200

    Improve startup scripts

--- bin/klab.bat
+++ bin/klab.bat
@@ -1,1 +1,18 @@
-:: TODO same logic as the sh script
+@echo off
+
+set JAVA=%userprofile%\.klab\jre16\bin\java
+set KLAB_RELEASE=develop
+set KLAB_VERSION=0.11.0-SNAPSHOT
+set CLI_PATH=%userprofile%\.klab\releases\%KLAB_RELEASE%\cli
+set CLI_PRODUCT=%CLI_PATH%\cli-%KLAB_VERSION%.jar
+
+IF NOT "%1"=="" (
+    IF "%1"=="-dev" (
+	    echo TODO ADD DEBUG OPTION
+	    echo TODO FIND CODE IN GIT AND REDIRECT CLI_PATH
+        SHIFT
+    )
+)
+
+%JAVA% -cp "%CLI_PATH%\lib\*;%CLI_PRODUCT%" -Xmx4096M org.integratedmodelling.klab.k.Main - "$@"
+@echo on
