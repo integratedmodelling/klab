f84a7973c54b85ea0ef6c82851ff6453a5474bbe
mario <mario@FerdBilbo>
2023-02-05 08:25:07 +0100

    Notes

--- api/org.integratedmodelling.klab.api/src/org/integratedmodelling/klab/api/engine/IEngineService.java
+++ api/org.integratedmodelling.klab.api/src/org/integratedmodelling/klab/api/engine/IEngineService.java
@@ -3,7 +3,9 @@ package org.integratedmodelling.klab.api.engine;
 import org.integratedmodelling.klab.api.auth.IUserIdentity;
 import org.integratedmodelling.klab.api.observations.IObservation;
 import org.integratedmodelling.klab.api.observations.IObserver;
+import org.integratedmodelling.klab.api.provenance.IArtifact;
 import org.integratedmodelling.klab.api.runtime.ISession;
+import org.integratedmodelling.klab.api.runtime.dataflow.IDataflow;
 
 /**
  * A redesign of the engine API and its subcomponents, eventually available as separate services.
@@ -15,7 +17,11 @@ import org.integratedmodelling.klab.api.runtime.ISession;
 public interface IEngineService {
 
     /**
-     * Scope for any observation operation. The destination of the observation depends on the scope.
+     * Scope for any observation operation. A root scope is given for the user upon authentication
+     * and more scopes can be asked from it to handle sessions, applications and contexts. This
+     * should eventually carry all user states and substitute ISessionState. Scopes are hierarchical
+     * and there is a scope per application, context and session (which becomes transparently
+     * handled).
      * 
      */
     interface ObservationScope {
@@ -47,11 +53,15 @@ public interface IEngineService {
 
     interface Resolver {
 
+        IDataflow<?> resolve(Object observable, ObservationScope scope);
+
     }
 
     interface Runtime {
 
-        ObservationScope createContextScope(IUserIdentity user, ObservationScope scope);
+        // ObservationScope createContextScope(IUserIdentity user, ObservationScope scope);
+
+        <T extends IArtifact> T run(IDataflow<T> dataflow, ObservationScope scope);
 
     }
 
