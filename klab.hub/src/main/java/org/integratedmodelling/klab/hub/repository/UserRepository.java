aa6f8d9421b0a0233db438b44c3f190fa9c0a9cb
Íñigo Cobián <inigo.cobian@gmail.com>
2023-02-13 11:36:39 +0100

    Added entrypoint to list all users with a role

--- klab.hub/src/main/java/org/integratedmodelling/klab/hub/repository/UserRepository.java
+++ klab.hub/src/main/java/org/integratedmodelling/klab/hub/repository/UserRepository.java
@@ -3,6 +3,7 @@ package org.integratedmodelling.klab.hub.repository;
 import java.util.List;
 import java.util.Optional;
 import org.bson.types.ObjectId;
+import org.integratedmodelling.klab.auth.Role;
 import org.integratedmodelling.klab.hub.api.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
@@ -27,4 +28,7 @@ public interface UserRepository extends MongoRepository<User, ObjectId>{
     
     @Query("{'groupEntries.group.$id' : ?0}")
 	List<User> getUsersByGroupEntriesWithGroupId(ObjectId id);
+    
+    @Query("{'roles' : ?0}")
+    List<User> getUsersByRole(Role role);
 }
