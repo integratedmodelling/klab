23845f56ee1f4780d78bc7fe702db74c3f2d2e2c
Enrico Girotto <enrico.girotto@bc3research.org>
2023-02-21 15:16:55 +0100

    Add CustomProperty object
    The same object will be used for IM-82 in user

---
+++ api/org.integratedmodelling.klab.api/src/org/integratedmodelling/klab/rest/CustomProperty.java
@@ -0,0 +1,54 @@
+package org.integratedmodelling.klab.rest;
+
+import java.util.Objects;
+
+/**
+ * Custom properties with visibility field
+ * @author Enrico Girotto
+ *
+ */
+public class CustomProperty {
+    
+    private String key;
+    private String value;
+    private boolean onlyAdmin;
+    
+    public String getKey() {
+        return key;
+    }
+    public void setKey(String key) {
+        this.key = key;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+    public boolean isOnlyAdmin() {
+        return onlyAdmin;
+    }
+    public void setOnlyAdmin(boolean onlyAdmin) {
+        this.onlyAdmin = onlyAdmin;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, onlyAdmin, value);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CustomProperty other = (CustomProperty) obj;
+        return Objects.equals(key, other.key) && onlyAdmin == other.onlyAdmin && Objects.equals(value, other.value);
+    }
+    @Override
+    public String toString() {
+        return "CustomProperty [key=" + key + ", value=" + value + ", onlyAdmin=" + onlyAdmin + "]";
+    }
+
+}
