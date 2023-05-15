a69750080c85810a5aecefde4d958f508aeaf6ec
Enrico Girotto <enrico.girotto@bc3research.org>
2023-05-15 15:50:25 +0200

    Change to logger, pushed to try in test environment

--- klab.hub/src/main/java/org/integratedmodelling/klab/hub/commands/GenerateHubReference.java
+++ klab.hub/src/main/java/org/integratedmodelling/klab/hub/commands/GenerateHubReference.java
@@ -8,9 +8,12 @@ import org.integratedmodelling.klab.auth.Hub;
 import org.integratedmodelling.klab.auth.Partner;
 import org.integratedmodelling.klab.rest.HubReference;
 import org.integratedmodelling.klab.rest.IdentityReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GenerateHubReference {
-	
+    protected static final Logger logger = LoggerFactory.getLogger(GenerateHubReference.class);
+    
 	public HubReference execute() {
 		Hub hub = Authentication.INSTANCE.getAuthenticatedIdentity(Hub.class);
 		IPartnerIdentity rootIdentity = Authentication.INSTANCE.getAuthenticatedIdentity(Partner.class);
@@ -19,13 +22,13 @@ public class GenerateHubReference {
 		partnerIdentity.setId(hub.getParentIdentity().getName());
 		partnerIdentity.setEmail(hub.getParentIdentity().getEmailAddress());
 		partnerIdentity.setLastLogin(LocalDateTime.now().toString());
-		System.out.println("PartnerIdentity: " + partnerIdentity);
+		logger.info("PartnerIdentity: " + partnerIdentity);
 		HubReference hubReference = new HubReference();
 		hubReference.setId(hub.getName());
 		hubReference.setOnline(true);
 		hubReference.getUrls().addAll(hub.getUrls());
 		hubReference.setPartner(partnerIdentity);
-		System.out.println("HubReference: " + hubReference);
+		logger.info("HubReference: " + hubReference);
 		return hubReference;
 	}
 }
