f5f14e2b93215b65aa3b83f6d73469190b558327
Ferdinando Villa <ferdinando.villa@gmail.com>
2023-05-15 14:23:37 +0200

    Finish authorization handler

--- klab.engine/src/main/java/org/integratedmodelling/klab/auth/Authorization.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/auth/Authorization.java
@@ -3,6 +3,7 @@ package org.integratedmodelling.klab.auth;
 import java.util.Base64;
 
 import org.integratedmodelling.klab.Authentication;
+import org.integratedmodelling.klab.exceptions.KlabAuthorizationException;
 import org.integratedmodelling.klab.exceptions.KlabIOException;
 import org.integratedmodelling.klab.rest.ExternalAuthenticationCredentials;
 
@@ -16,6 +17,9 @@ import kong.unirest.json.JSONObject;
  * This is returned after authentication from a service using ExternalCredentials. Handles any
  * refresh automatically according to the scheme.
  * 
+ * {@link #getAuthorization()} returns the token complete with token type ("Bearer", "Basic") and
+ * any prefix passed to the constructor, ready to be used as an HTTP Authorization header.
+ * 
  * @author Ferd
  *
  */
@@ -25,6 +29,7 @@ public class Authorization {
     private String token;
     private long expiry = -1;
     private String prefix;
+    private String tokenType;
 
     public Authorization(ExternalAuthenticationCredentials credentials, String prefix) {
         this(credentials);
@@ -32,13 +37,14 @@ public class Authorization {
     }
 
     public Authorization(ExternalAuthenticationCredentials credentials) {
-        
+
         this.credentials = credentials;
 
         if ("basic".equals(credentials.getScheme())) {
+            this.tokenType = "Basic";
             byte[] encodedBytes = Base64.getEncoder()
                     .encode((credentials.getCredentials().get(0) + ":" + credentials.getCredentials().get(1)).getBytes());
-            this.token = "Basic " + new String(encodedBytes);
+            this.token = new String(encodedBytes);
         } else if ("oauth2".equals(credentials.getScheme())) {
             refreshToken();
         }
@@ -54,7 +60,8 @@ public class Authorization {
                 .field("client_secret", credentials.getCredentials().get(3)).field("scope", credentials.getCredentials().get(4));
 
         if (this.token != null) {
-            query = query.header("Authorization:", token);
+            query = query.header("Authorization:",
+                    (tokenType == null ? "" : (tokenType + " ")) + (prefix == null ? "" : prefix) + token);
         }
 
         this.expiry = System.currentTimeMillis();
@@ -62,6 +69,9 @@ public class Authorization {
         if (result.isSuccess()) {
             JSONObject response = result.getBody().getObject();
             long duration = -1;
+            if (response.has("token_type")) {
+                this.tokenType = response.getString("token_type");
+            }
             if (response.has("refresh_token")) {
                 this.token = response.getString("refresh_token");
                 this.expiry = this.expiry + response.getLong("");
@@ -91,22 +101,25 @@ public class Authorization {
             if (this.expiry <= System.currentTimeMillis()) {
                 refreshToken();
             }
-            return prefix == null ? token : (prefix + token);
+            // repeat
+            if (token == null) {
+                throw new KlabIOException("Authorization failed");
+            }
         }
 
-        // repeat
-        if (token == null) {
-            throw new KlabIOException("Authorization failed");
-        }
-
-        return token;
+        return (tokenType == null ? "" : (tokenType + " ")) + (prefix == null ? token : (prefix + token));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ExternalAuthenticationCredentials crds = Authentication.INSTANCE.getCredentials("https://openeo.vito.be");
         if (crds != null) {
             Authorization authorization = new Authorization(crds, "oidc/terrascope/");
             System.out.println(authorization.getAuthorization());
+            System.out.println("Sleeping 300 seconds: don't change the channel");
+            Thread.sleep(300000l);
+            System.out.println(authorization.getAuthorization());
+        } else {
+            throw new KlabAuthorizationException("no stored credentials for https://openeo.vito.be");
         }
     }
 
