72f127ee881a27e767bb04e35dae5157cfe4e3b0
Enrico Girotto <enrico.girotto@bc3research.org>
2023-06-23 10:46:58 +0200

    ObjectMapper issue solved

--- klab.hub/src/main/java/org/integratedmodelling/klab/hub/commands/GetNodesGroups.java
+++ klab.hub/src/main/java/org/integratedmodelling/klab/hub/commands/GetNodesGroups.java
@@ -7,16 +7,18 @@ import org.integratedmodelling.klab.hub.api.MongoGroup;
 import org.integratedmodelling.klab.hub.api.MongoNode;
 import org.integratedmodelling.klab.rest.Group;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GetNodesGroups {
 	
 	private MongoNode node;
-	private ObjectMapper objectMapper;
+	private ObjectMapper objectMapper = new ObjectMapper();
 	
 	public GetNodesGroups(MongoNode node) {
 		super();
 		this.node = node;
+		this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
 
 	public Set<Group> execute() {
