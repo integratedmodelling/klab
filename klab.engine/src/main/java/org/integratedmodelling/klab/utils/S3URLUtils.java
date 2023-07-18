1cea7061f5a9dced13c983f871e169dd5d823e80
Íñigo Cobián <inigo.cobian@gmail.com>
2023-07-18 17:37:43 +0200

    Small refactor and cleanup at S3URLUtils

--- klab.engine/src/main/java/org/integratedmodelling/klab/utils/S3URLUtils.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/utils/S3URLUtils.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.annotation.CheckReturnValue;
-
 import org.integratedmodelling.klab.exceptions.KlabIOException;
 import org.integratedmodelling.klab.exceptions.KlabResourceAccessException;
 import org.junit.Test;
@@ -22,49 +20,47 @@ import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 public class S3URLUtils {
-	@CheckReturnValue
-	public static File getFileForURL(String url, String region) throws KlabIOException {
-		if (url.toString().startsWith("s3:")) {
-			Pair<String, String> bucketAndKey = extractBucketAndKey(url);
-			String bucketName = bucketAndKey.getFirst();
-			String key = bucketAndKey.getSecond();
-			try {
-				AmazonS3 s3client = AmazonS3ClientBuilder.standard()
-					.withRegion(Regions.fromName(region))
-					.withCredentials(new ProfileCredentialsProvider())
-					.build();
+    public static File getFileForURL(String url, String region) {
+        if (url.toString().startsWith("s3:")) {
+            Pair<String, String> bucketAndKey = extractBucketAndKey(url);
+            String bucketName = bucketAndKey.getFirst();
+            String key = bucketAndKey.getSecond();
+            try {
+                AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+                    .withRegion(Regions.fromName(region))
+                    .withCredentials(new ProfileCredentialsProvider())
+                    .build();
 
-				S3Object s3object = s3client.getObject(bucketName, key);
-				S3ObjectInputStream inputStream = s3object.getObjectContent();
-				File temp = File.createTempFile("url", "url");
-				FileUtils.copyInputStreamToFile(inputStream, temp);
-				return temp;
-			} catch (SdkClientException e) {
-				throw new KlabResourceAccessException(e);
-			} catch (IOException e) {
-				throw new KlabIOException(e);
-			}
-		} else {
-			try {
-				return URLUtils.getFileForURL(new URL(url));
-			} catch (KlabIOException | MalformedURLException e) {
-				throw new KlabIOException(e);
-			}
-		}
-	}
+                S3Object s3object = s3client.getObject(bucketName, key);
+                S3ObjectInputStream inputStream = s3object.getObjectContent();
+                File temp = File.createTempFile("url", "url");
+                FileUtils.copyInputStreamToFile(inputStream, temp);
+                return temp;
+            } catch (SdkClientException e) {
+                throw new KlabResourceAccessException(e);
+            } catch (IOException e) {
+                throw new KlabIOException(e);
+            }
+        }
+        try {
+            return URLUtils.getFileForURL(new URL(url));
+        } catch (KlabIOException | MalformedURLException e) {
+            throw new KlabIOException(e);
+        }
+    }
 
-	private static Pair<String, String> extractBucketAndKey(String s3Uri) {
+    private static Pair<String, String> extractBucketAndKey(String s3Uri) {
         String[] uriParts = s3Uri.replaceFirst("s3://", "").split("/", 2);
         return new Pair<>(uriParts[0], uriParts[1]);
-	}
+    }
 
-	@Test
-	public void testerino() throws KlabIOException, IOException {
-		// We are using this URL because it is a known open S3 resource
-		String S3_URL = "s3://landsat-pds/scene_list.gz";
-		
-		File file = S3URLUtils.getFileForURL(S3_URL, "us-west-2");
+    @Test
+    public void testerino() throws KlabIOException, IOException {
+        // We are using this URL because it is a known open S3 resource
+        String S3_URL = "s3://landsat-pds/scene_list.gz";
 
-		assertTrue(file.exists());
-	}
+        File file = S3URLUtils.getFileForURL(S3_URL, "us-west-2");
+
+        assertTrue(file.exists());
+    }
 }
