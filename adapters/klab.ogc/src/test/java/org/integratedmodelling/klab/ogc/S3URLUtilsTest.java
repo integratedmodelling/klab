0e3f44ce27f97af28f712080e7f60d627c6985c3
Íñigo Cobián <inigo.cobian@gmail.com>
2023-07-24 15:45:40 +0200

    Segregate Minio Exploratory tests from the other S3 tests

--- adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/S3URLUtilsTest.java
+++ adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/S3URLUtilsTest.java
@@ -1,15 +1,19 @@
 package org.integratedmodelling.klab.ogc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.Optional;
 
 import org.integratedmodelling.klab.S3URLUtils;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import io.minio.MinioClient;
@@ -22,7 +26,6 @@ import io.minio.errors.XmlParserException;
 import io.minio.messages.Bucket;
 
 public class S3URLUtilsTest {
-    String existingBucketName = "grocery";
     // These endpoint and keys come from the MinIO documentation
     String minioEndpoint = "https://play.min.io";
     String accessKey = "Q3AM3UQ867SPQQA43P2F";
@@ -36,26 +39,45 @@ public class S3URLUtilsTest {
         
         File file = S3URLUtils.getFileForURL(S3_URL, "us-west-2");
 
-        assertEquals(true, file.exists());
+        assertTrue(file.exists());
     }
 
-    @Test
-    public void makeATestConnection() {
-        MinioClient minioClient = MinioClient.builder()
-                .endpoint(minioEndpoint)
-                .credentials(accessKey, secretKey)
-                .build();
-    }
+    @Nested
+    @DisplayName("Minio Exploratory Tests")
+    public class MinioExploratoryTests {
+        @Test
+        public void makeATestConnection() {
+            @SuppressWarnings("unused")
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint(minioEndpoint)
+                    .credentials(accessKey, secretKey)
+                    .build();
+        }
 
-    @Test
-    public void getBuckets() throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
-        MinioClient minioClient = MinioClient.builder()
-                .endpoint(minioEndpoint)
-                .credentials(accessKey, secretKey)
-                .build();
+        @Test
+        public void getBuckets() throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint(minioEndpoint)
+                    .credentials(accessKey, secretKey)
+                    .build();
 
-        List<Bucket> buckets = minioClient.listBuckets();
+            List<Bucket> buckets = minioClient.listBuckets();
+        
+            assertFalse(buckets.isEmpty());
+        }
 
-        assertEquals(false, buckets.isEmpty());
+        @Test
+        public void knownBucketExists() throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
+            String existingBucketName = "grocery";
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint(minioEndpoint)
+                    .credentials(accessKey, secretKey)
+                    .build();
+
+            List<Bucket> buckets = minioClient.listBuckets();
+            Optional<Bucket> bucket = buckets.stream().filter(b -> b.name().equals(existingBucketName)).findFirst();
+
+            assertTrue(bucket.isPresent());
+        }
     }
 }
