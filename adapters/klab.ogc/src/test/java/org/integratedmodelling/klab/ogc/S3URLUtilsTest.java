0efd5f2abe651966d4b001352c9596e5ca1d0f69
Íñigo Cobián <inigo.cobian@gmail.com>
2023-07-26 15:45:22 +0200

    Add tests to show how to download files from AWS

--- adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/S3URLUtilsTest.java
+++ adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/S3URLUtilsTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import io.minio.DownloadObjectArgs;
 import io.minio.MinioClient;
 import io.minio.errors.ErrorResponseException;
 import io.minio.errors.InsufficientDataException;
@@ -30,17 +34,6 @@ public class S3URLUtilsTest {
     String minioEndpoint = "https://play.min.io";
     String accessKey = "Q3AM3UQ867SPQQA43P2F";
     String secretKey = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG";
-
-    @Test
-    @Disabled("Not finished yet")
-    public void getAFile() {
-        // We are using this URL because it is a known open S3 resource
-        String S3_URL = "s3://landsat-pds/scene_list.gz";
-        
-        File file = S3URLUtils.getFileForURL(S3_URL, "us-west-2");
-
-        assertTrue(file.exists());
-    }
 
     @Nested
     @DisplayName("Minio Exploratory Tests")
@@ -78,6 +71,47 @@ public class S3URLUtilsTest {
             Optional<Bucket> bucket = buckets.stream().filter(b -> b.name().equals(existingBucketName)).findFirst();
 
             assertTrue(bucket.isPresent());
+        }
+    }
+
+    @Nested
+    @DisplayName("AWS Exploratory Tests")
+    public class AWSExploratoryTests {
+        String bucketRegion = "us-west-2";
+        String awsEndpoint = "https://s3.amazonaws.com";
+        String testEndpoint = "s3://landsat-pds/scene_list.gz";
+        // TODO Do not hardcode the credentials
+        String awsAccessKey = "XXX";
+        String awsSecretKey = "XXX";
+
+        @Test
+        public void makeATestConnection() {
+            @SuppressWarnings("unused")
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint(minioEndpoint)
+                    .credentials(accessKey, secretKey)
+                    .build();
+        }
+
+        @Test
+        public void downloadFile() throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
+            String fileDestination = "./scene_list.gz";
+            String bucket = "landsat-pds";
+            String object = "scene_list.gz";
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint(awsEndpoint)
+                    .credentials(awsAccessKey, awsSecretKey)
+                    .region(bucketRegion)
+                    .build();
+
+            minioClient.downloadObject(DownloadObjectArgs.builder()
+                    .bucket(bucket)
+                    .filename(fileDestination)
+                    .object(object)
+                    .build());
+
+            Path path = Paths.get(fileDestination);
+            assertTrue(Files.exists(path));
         }
     }
 }
