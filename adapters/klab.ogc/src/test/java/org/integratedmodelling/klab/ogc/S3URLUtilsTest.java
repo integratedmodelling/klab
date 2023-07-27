6102ba3112cd8507b07295748abb7c43a648c58f
Íñigo Cobián <inigo.cobian@gmail.com>
2023-07-27 17:10:07 +0200

    Clean tests to connect to S3 clients via a method call

--- adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/S3URLUtilsTest.java
+++ adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/S3URLUtilsTest.java
@@ -3,7 +3,6 @@ package org.integratedmodelling.klab.ogc;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,7 +13,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.integratedmodelling.klab.S3URLUtils;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -30,42 +28,30 @@ import io.minio.errors.XmlParserException;
 import io.minio.messages.Bucket;
 
 public class S3URLUtilsTest {
-    // These endpoint and keys come from the MinIO documentation
-    String minioEndpoint = "https://play.min.io";
-    String accessKey = "Q3AM3UQ867SPQQA43P2F";
-    String secretKey = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG";
-
     @Nested
     @DisplayName("Minio Exploratory Tests")
     public class MinioExploratoryTests {
+        String minioEndpoint = "https://play.min.io";
+
         @Test
         public void makeATestConnection() {
             @SuppressWarnings("unused")
-            MinioClient minioClient = MinioClient.builder()
-                    .endpoint(minioEndpoint)
-                    .credentials(accessKey, secretKey)
-                    .build();
+            MinioClient minioClient = S3URLUtils.connect(minioEndpoint, Optional.empty());
         }
 
         @Test
         public void getBuckets() throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
-            MinioClient minioClient = MinioClient.builder()
-                    .endpoint(minioEndpoint)
-                    .credentials(accessKey, secretKey)
-                    .build();
+            MinioClient minioClient = S3URLUtils.connect(minioEndpoint, Optional.empty());
 
             List<Bucket> buckets = minioClient.listBuckets();
-        
+
             assertFalse(buckets.isEmpty());
         }
 
         @Test
         public void knownBucketExists() throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
             String existingBucketName = "grocery";
-            MinioClient minioClient = MinioClient.builder()
-                    .endpoint(minioEndpoint)
-                    .credentials(accessKey, secretKey)
-                    .build();
+            MinioClient minioClient = S3URLUtils.connect(minioEndpoint, Optional.empty());
 
             List<Bucket> buckets = minioClient.listBuckets();
             Optional<Bucket> bucket = buckets.stream().filter(b -> b.name().equals(existingBucketName)).findFirst();
@@ -77,32 +63,20 @@ public class S3URLUtilsTest {
     @Nested
     @DisplayName("AWS Exploratory Tests")
     public class AWSExploratoryTests {
-        String bucketRegion = "us-west-2";
-        String awsEndpoint = "https://s3.amazonaws.com";
-        String testEndpoint = "s3://landsat-pds/scene_list.gz";
-        // TODO Do not hardcode the credentials
-        String awsAccessKey = "XXX";
-        String awsSecretKey = "XXX";
-
         @Test
         public void makeATestConnection() {
             @SuppressWarnings("unused")
-            MinioClient minioClient = MinioClient.builder()
-                    .endpoint(minioEndpoint)
-                    .credentials(accessKey, secretKey)
-                    .build();
+            MinioClient minioClient = S3URLUtils.connect(S3URLUtils.AWS_ENDPOINT, Optional.empty());
         }
 
         @Test
         public void downloadFile() throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
+            // TODO put this file in a proper place
             String fileDestination = "./scene_list.gz";
             String bucket = "landsat-pds";
             String object = "scene_list.gz";
-            MinioClient minioClient = MinioClient.builder()
-                    .endpoint(awsEndpoint)
-                    .credentials(awsAccessKey, awsSecretKey)
-                    .region(bucketRegion)
-                    .build();
+            String bucketRegion = "us-west-2";
+            MinioClient minioClient = S3URLUtils.connect(S3URLUtils.AWS_ENDPOINT, Optional.of(bucketRegion));
 
             minioClient.downloadObject(DownloadObjectArgs.builder()
                     .bucket(bucket)
