c83eed77b7aeae5a7779b826a04fe0fbd736c05b
Íñigo Cobián <inigo.cobian@gmail.com>
2023-07-28 14:58:24 +0200

    Renamed S3 tests file to S3ConnectionManagerTest

--- adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/S3ConnectionManagerTest.java
+++ adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/S3ConnectionManagerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class S3URLUtilsTest {
+public class S3ConnectionManagerTest {
     String minioEndpoint = "https://play.min.io";
 
     @Test
