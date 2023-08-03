e90be88e83a66650cd2572f7764faeb03ff9f780
Diego <diego.bengocheapaz@pm.me>
2023-08-03 11:16:17 +0200

    Create resource and contextualize

--- components/klab.component.random/src/main/java/org/integratedmodelling/random/adapters/RecreationIDBAdapter.java
+++ components/klab.component.random/src/main/java/org/integratedmodelling/random/adapters/RecreationIDBAdapter.java
@@ -1,6 +1,7 @@
 package org.integratedmodelling.random.adapters;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -19,8 +20,11 @@ import org.integratedmodelling.klab.api.observations.scale.IScale;
 import org.integratedmodelling.klab.api.observations.scale.space.IShape;
 import org.integratedmodelling.klab.api.provenance.IArtifact.Type;
 import org.integratedmodelling.klab.api.runtime.IContextualizationScope;
+import org.integratedmodelling.klab.common.Geometry;
 import org.integratedmodelling.klab.components.geospace.extents.Projection;
 import org.integratedmodelling.klab.components.geospace.extents.Shape;
+import org.integratedmodelling.klab.data.resources.Resource;
+import org.integratedmodelling.klab.rest.ResourceReference;
 import org.integratedmodelling.klab.scale.Scale;
 
 @UrnAdapter(type = "random", version = Version.CURRENT)
@@ -35,6 +39,13 @@ public class RecreationIDBAdapter implements IUrnAdapter {
 	public static final String RADIUS = "radius";
 	public static final String APIKEY = "apikey"; 
 	
+	
+	public static String[] object_attribute_ids = new String[] { LIMIT, OFFSET, STATE, ACTIVITY, RADIUS};
+	
+	public RecreationIDBAdapter() {
+		Arrays.sort(object_attribute_ids);
+	}
+	
 	@Override
 	public String getName() {
 		return NAME;
@@ -42,7 +53,7 @@ public class RecreationIDBAdapter implements IUrnAdapter {
 	
 	@Override
 	public boolean isOnline(Urn urn) {
-		// TODO Auto-generated method stub
+		//TODO
 		return false;
 	}
 	
@@ -82,12 +93,12 @@ public class RecreationIDBAdapter implements IUrnAdapter {
 				? urn.getParameters().get(APIKEY) 
 				: "82b00cad-58b5-40e8-9d77-77caba299473";
 		
-		String artifactName = urn.getResourceId().substring(0, urn.getResourceId().length() - 1);
-
         IScale scale = geometry instanceof IScale ? (IScale) geometry : Scale.create(geometry);
 
         int n = 1;
         if (scale.getSpace() != null) {
+        	
+        	String artifactName = urn.getResourceId().substring(0, urn.getResourceId().length() - 1);
         	
         	RecreationIDB ridb = new RecreationIDB();
             String input = buildRecreationIDBInput(parameters);
@@ -101,7 +112,8 @@ public class RecreationIDBAdapter implements IUrnAdapter {
             	double lat = (double) area.get("lat");
             	double lon = (double) area.get("lon");
             	
-            	// Create the point
+            	// Create the point. 
+            	// TODO: in the general case recreation areas should be polygons while entrances to the areas are points.
             	shape = Shape.create(lon,lat,(Projection) scope.getScale().getSpace().getProjection());
             	
             	Builder obuilder = builder.startObject(scope.getTargetName(), artifactName + "_" + (n++),
@@ -130,7 +142,6 @@ public class RecreationIDBAdapter implements IUrnAdapter {
 
 	
 	private IGeometry makeScale(Urn urn, IShape shape, IContextualizationScope scope) {
-
         List<IExtent> extents = new ArrayList<>();
         extents.add(scope.getContextSubject().getScale().getTime());
         extents.add(shape);
@@ -139,42 +150,46 @@ public class RecreationIDBAdapter implements IUrnAdapter {
 	
 	@Override
 	public IResource getResource(String urn) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Urn kurn = new Urn(urn);
+        ResourceReference ref = new ResourceReference();
+        ref.setUrn(urn.toString());
+        ref.setAdapterType(getName());
+        ref.setLocalName(kurn.getResourceId());
+        ref.setGeometry("#S2");
+	    ref.setVersion(Version.CURRENT);
+	    ref.setType(getType(kurn));
+	    return new Resource(ref);
 	}
 
 	@Override
 	public IResource contextualize(IResource resource, IGeometry scale, IGeometry overallScale, IObservable semantics) {
-		// TODO Auto-generated method stub
-		return null;
+		return resource;
 	}
 
 	
-
-	
-
 	@Override
 	public Type getType(Urn urn) {
-		// TODO Auto-generated method stub
-		return null;
+		// Instances of recreation areas are all bounded objects: polygons or points. 
+		return Type.OBJECT;
 	}
 
 	@Override
 	public IGeometry getGeometry(Urn urn) {
-		// TODO Auto-generated method stub
-		return null;
+		// Object geometry.
+		return Geometry.create("#S2");
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Outdoor recreation areas in USA federal lands.";
 	}
 
 	@Override
 	public Collection<String> getResourceUrns() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> ret = new ArrayList<>();
+        // TODO
+        return ret;
 	}
 	
 	
