3f5a3139bad8b45a8db0fa6dcdd783445a725dd3
Íñigo Cobián <inigo.cobian@gmail.com>
2023-11-09 10:30:46 +0100

    Check media support case and whitespace insensitive
    Also, support deprecated legacy COGs

--- adapters/klab.ogc/src/main/java/org/integratedmodelling/klab/stac/STACAssetParser.java
+++ adapters/klab.ogc/src/main/java/org/integratedmodelling/klab/stac/STACAssetParser.java
@@ -5,13 +5,17 @@ import java.util.Set;
 import kong.unirest.json.JSONObject;
 
 public class STACAssetParser {
-    final private static Set<String> SUPPORTED_MEDIA_TYPE = Set.of("image/tiff; application=geotiff; profile=cloud-optimized");
+    // https://github.com/radiantearth/stac-spec/blob/master/best-practices.md#common-media-types-in-stac
+    final private static Set<String> SUPPORTED_MEDIA_TYPE = Set.of("image/tiff;application=geotiff;profile=cloud-optimized","image/vnd.stac.geotiff;profile=cloud-optimized");
 
     public static String readTitle(JSONObject collection) {
         return collection.has("title") ? collection.getString("title") : collection.getString("id");
     }
 
     public static boolean isSupportedMediaType(JSONObject asset) {
-        return asset.has("type") && SUPPORTED_MEDIA_TYPE.contains(asset.getString("type"));
+        if (!asset.has("type")) {
+            return false;
+        }
+        return SUPPORTED_MEDIA_TYPE.contains(asset.getString("type").replace(" ", "").toLowerCase());
     }
 }
