be11637b330c766d9fd35a4012f5beee88287a3c
Íñigo Cobián <inigo.cobian@gmail.com>
2024-01-10 14:44:06 +0100

    Added tests for STACValidator.canHandle

---
+++ adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/stac/test/STACValidatorTest.java
@@ -0,0 +1,46 @@
+package org.integratedmodelling.klab.ogc.stac.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.integratedmodelling.kim.api.IParameters;
+import org.integratedmodelling.klab.stac.STACValidator;
+import org.integratedmodelling.klab.utils.Parameters;
+import org.junit.jupiter.api.Test;
+
+public class STACValidatorTest {
+    @Test
+    public void canHandle_passValidUserData() {
+        STACValidator validator = new STACValidator();
+        IParameters<String> userData = new Parameters<>();
+        userData.put("catalogUrl", "https://planetarycomputer.microsoft.com/api/stac/v1");
+        userData.put("collectionId", "io-lulc-9-class");
+        userData.put("asset", "data");
+
+        boolean ret = validator.canHandle(null, userData);
+
+        assertTrue(ret);
+    }
+
+    @Test
+    public void canHandle_failEmptyUserData() {
+        STACValidator validator = new STACValidator();
+        IParameters<String> userData = new Parameters<>();
+
+        boolean ret = validator.canHandle(null, userData);
+
+        assertFalse(ret);
+    }
+
+    @Test
+    public void canHandle_failIncompleteUserData() {
+        STACValidator validator = new STACValidator();
+        IParameters<String> userData = new Parameters<>();
+        userData.put("catalogUrl", "https://planetarycomputer.microsoft.com/api/stac/v1");
+        // Missing "collectionId" and "asset"
+
+        boolean ret = validator.canHandle(null, userData);
+
+        assertFalse(ret);
+    }
+}
