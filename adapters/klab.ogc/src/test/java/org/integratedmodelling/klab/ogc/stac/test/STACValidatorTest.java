ae567b02bdf49e0faecc8e8a38caf85e66850d6b
Íñigo Cobián <inigo.cobian@gmail.com>
2024-01-11 10:40:19 +0100

    Add tests for STACValidator.validate

--- adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/stac/test/STACValidatorTest.java
+++ adapters/klab.ogc/src/test/java/org/integratedmodelling/klab/ogc/stac/test/STACValidatorTest.java
@@ -4,11 +4,38 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.integratedmodelling.kim.api.IParameters;
+import org.integratedmodelling.klab.api.data.IResource.Builder;
 import org.integratedmodelling.klab.stac.STACValidator;
 import org.integratedmodelling.klab.utils.Parameters;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class STACValidatorTest {
+    @Test
+    public void validate_passCreateValidBuilder() {
+        STACValidator validator = new STACValidator();
+        IParameters<String> userData = new Parameters<>();
+        userData.put("catalogUrl", "https://planetarycomputer.microsoft.com/api/stac/v1");
+        userData.put("collectionId", "io-lulc-9-class");
+        userData.put("asset", "data");
+
+        Builder ret =  validator.validate("any:urn", null, userData, null);
+
+        assertFalse(ret.hasErrors());
+    }
+
+    @Test
+    public void validate_failIncompleteUserData() {
+        STACValidator validator = new STACValidator();
+        IParameters<String> userData = new Parameters<>();
+        userData.put("catalogUrl", "https://planetarycomputer.microsoft.com/api/stac/v1");
+        userData.put("collectionId", "io-lulc-9-class");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            validator.validate("any:urn", null, userData, null);
+        });
+    }
+
     @Test
     public void canHandle_passValidUserData() {
         STACValidator validator = new STACValidator();
