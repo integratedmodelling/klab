9b610d289c848e99017cf7af09427fb2ff5d8ec4
Enrico Girotto <enrico.girotto@bc3research.org>
2024-01-17 15:03:50 +0100

    Part of previous commit

--- klab.engine/src/main/java/org/integratedmodelling/klab/components/runtime/actors/ViewBehavior.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/components/runtime/actors/ViewBehavior.java
@@ -675,7 +675,7 @@ public class ViewBehavior {
 
         private String appId;
         // add to this
-//        private ViewComponent group;
+        private ViewComponent group;
         // keep this for resetting
         private ViewComponent originalGroup;
 
@@ -683,8 +683,8 @@ public class ViewBehavior {
                 ActorRef<KlabMessage> sender, String callId) {
             super(identity, null, scope, sender, callId);
             this.appId = appId;
-            this.component = copyComponent(scope.getViewScope().getCurrentComponent());
-            this.originalGroup = copyComponent(this.component);
+            this.group = copyComponent(scope.getViewScope().getCurrentComponent());
+            this.originalGroup = copyComponent(this.group);
         }
 
         @Override
@@ -696,7 +696,7 @@ public class ViewBehavior {
                     arg = ((KActorsValue) arg).evaluate(scope, identity, false);
                 }
                 if (arg instanceof Constructor) {
-                    this.sender.tell(new AddComponentToGroup(this.component, ((Constructor) arg).getComponent(),
+                    this.sender.tell(new AddComponentToGroup(this.group, ((Constructor) arg).getComponent(),
                             ((Constructor) arg).getArguments(), scope));
                 }
             } else if ("remove".equals(message.getMessage())) {
