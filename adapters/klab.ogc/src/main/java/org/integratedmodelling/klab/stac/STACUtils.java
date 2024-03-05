424fe3b2e5bf91c9fdd50162658bc4511df91022
Íñigo Cobián <inigo.cobian@gmail.com>
2024-03-05 09:10:25 +0100

    Read STAC keywords without brackets

--- adapters/klab.ogc/src/main/java/org/integratedmodelling/klab/stac/STACUtils.java
+++ adapters/klab.ogc/src/main/java/org/integratedmodelling/klab/stac/STACUtils.java
@@ -1,7 +1,9 @@
 package org.integratedmodelling.klab.stac;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.integratedmodelling.klab.Version;
@@ -25,8 +27,9 @@ public class STACUtils {
         if (!json.has("keywords")) {
             return null;
         }
-        JSONArray keywords = json.getJSONArray("keywords");
-        return keywords.isEmpty() ? null : keywords.toString().replace("\"", "");
+        List<String> keywords = json.getJSONArray("keywords").toList();
+        return keywords.isEmpty() ? null :
+            keywords.stream().collect(Collectors.joining(","));
     }
 
     final private static Set<String> DOI_KEYS_IN_STAC_JSON = Set.of("sci:doi", "assets.sci:doi", "summaries.sci:doi", "properties.sci:doi", "item_assets.sci:doi");
