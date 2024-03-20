4c56cfff78ef4983312914219374a781bb62d7be
Íñigo Cobián <inigo.cobian@gmail.com>
2024-03-20 09:44:46 +0100

    Update S3 test and disable those that might not be replicable

--- klab.engine/src/test/java/org/integratedmodelling/klab/test/utils/S3ConnectionManagerTest.java
+++ klab.engine/src/test/java/org/integratedmodelling/klab/test/utils/S3ConnectionManagerTest.java
@@ -6,11 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Optional;
-
 import org.integratedmodelling.klab.utils.FileUtils;
 import org.integratedmodelling.klab.utils.s3.S3ConnectionManager;
 import org.integratedmodelling.klab.utils.s3.S3URLUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,7 +22,7 @@ public class S3ConnectionManagerTest {
     @Test
     public void makeATestConnection() {
         S3ConnectionManager s3connection = new S3ConnectionManager();
-        s3connection.connect(minioEndpoint, Optional.empty());
+        s3connection.connect(minioEndpoint);
     }
 
     @Test
@@ -38,7 +37,7 @@ public class S3ConnectionManagerTest {
     @Test
     public void isConnectedAfterConnection() {
         S3ConnectionManager s3connection = new S3ConnectionManager();
-        s3connection.connect(minioEndpoint, Optional.empty());
+        s3connection.connect(minioEndpoint);
 
         boolean isConnected = s3connection.isConnected();
 
@@ -46,21 +45,21 @@ public class S3ConnectionManagerTest {
     }
 
     @Nested
+    @Disabled("There are no known resources that will remain unmodified forever.")
     @DisplayName("Tests usign AWS")
     public class AWSTests {
         @Test
         public void makeATestConnection() {
             S3ConnectionManager s3connection = new S3ConnectionManager();
-            s3connection.connect(S3URLUtils.AWS_ENDPOINT, Optional.empty());
+            s3connection.connect(S3URLUtils.AWS_ENDPOINT);
         }
 
         @Test
         public void downloadFile() throws IOException {
             String testResourceURL = "s3://landsat-pds/scene_list.gz";
-            String bucketRegion = "us-west-2";
             String filePath = "test.gz";
             S3ConnectionManager s3connection = new S3ConnectionManager();
-            s3connection.connect(S3URLUtils.AWS_ENDPOINT, Optional.of(bucketRegion));
+            s3connection.connect(S3URLUtils.AWS_ENDPOINT);
 
             File file = s3connection.downloadFileFromS3URL(testResourceURL, filePath);
 
@@ -71,9 +70,8 @@ public class S3ConnectionManagerTest {
         @Test
         public void getStream() throws IOException {
             String testResourceURL = "s3://landsat-pds/scene_list.gz";
-            String bucketRegion = "us-west-2";
             S3ConnectionManager s3connection = new S3ConnectionManager();
-            s3connection.connect(S3URLUtils.AWS_ENDPOINT, Optional.of(bucketRegion));
+            s3connection.connect(S3URLUtils.AWS_ENDPOINT);
 
             InputStream input = s3connection.getInputStreamFromS3URL(testResourceURL);
 
