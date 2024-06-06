d39ee4c1a6e19808879b5b101fd805420cb9df74
Íñigo Cobián <inigo.cobian@gmail.com>
2024-06-06 17:34:43 +0200

    Update HortonMachine version

--- adapters/klab.ogc/src/main/java/org/integratedmodelling/klab/stac/STACEncoder.java
+++ adapters/klab.ogc/src/main/java/org/integratedmodelling/klab/stac/STACEncoder.java
@@ -215,7 +215,7 @@ public class STACEncoder implements IResourceEncoder {
             // data.
             final boolean allowTransform = true;
             String assetId = resource.getParameters().get("asset", String.class);
-            HMRaster outRaster = HMStacCollection.readRasterBandOnRegion(regionTransformed, assetId, items, allowTransform, mergeMode, lpm);
+            HMRaster outRaster = collection.readRasterBandOnRegion(regionTransformed, assetId, items, allowTransform, mergeMode, lpm);
             coverage = outRaster.buildCoverage();
         } catch (Exception e) {
             throw new KlabInternalErrorException("Cannot build STAC raster output. Reason " + e.getMessage());
