49133ea1feed6ed5fa2ed9ac608b46fc6a051d6c
Kristina <kristina.sanchez@bc3research.org>
2024-06-13 11:35:14 +0200

    feature: IM-393 Add Create tag notification REST call

---
+++ klab.hub/src/main/java/org/integratedmodelling/klab/hub/tags/enums/ITagElementEnum.java
@@ -0,0 +1,12 @@
+package org.integratedmodelling.klab.hub.tags.enums;
+
+/**
+ * Use for determinate entity of MongoTag, if is NULL is generic
+ * 
+ * @author kristina
+ *
+ */
+public enum ITagElementEnum {
+    USER, GROUP;
+
+}
