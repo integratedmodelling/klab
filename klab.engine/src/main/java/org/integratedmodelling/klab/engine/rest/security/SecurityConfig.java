9e4ffa21d4f2b4df9cb5c315d11881fe6c9e3bdb
Iván Perdomo <ivan@perdomo.me>
2024-10-18 12:32:21 +0200

    IM-459 Configure 'Remote Engine' as OAuth2 resource server

--- klab.engine/src/main/java/org/integratedmodelling/klab/engine/rest/security/SecurityConfig.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/engine/rest/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
@@ -18,6 +19,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
@@ -27,76 +29,77 @@ import org.springframework.security.web.authentication.preauth.PreAuthenticatedA
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-  @Autowired
-  private PreauthenticatedUserDetailsService customUserDetailsService;
+class SecurityConfig {
 
-  @Autowired
-  private EngineDirectoryAuthenticationProvider authProvider;
-  
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-	  http
-	  // disable automatic session creation to avoid use of cookie session
-	  // and the consequent authentication failures in web ui
-	  .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	  .and()
-	  .addFilterBefore(certFilter(), RequestHeaderAuthenticationFilter.class)
-//	  .authorizeRequests().anyRequest().hasAnyRole("ADMIN")
-//      .and()
-      .authorizeRequests().antMatchers("/login**").permitAll()
-      .and()
-      .formLogin().permitAll()
-      .and()
-      .logout().permitAll() 
-      .and()
-      .csrf().disable()
-      .exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+	@Autowired
+	private PreauthenticatedUserDetailsService customUserDetailsService;
+
+	@Autowired
+	private EngineDirectoryAuthenticationProvider authProvider;
+
+	@Profile("local")
+	@EnableWebSecurity
+	@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+	public class CertificateSecurityConfig extends WebSecurityConfigurerAdapter {
+		// disable automatic session creation to avoid use of cookie session
+		// and the consequent authentication failures in web ui
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+					.addFilterBefore(certFilter(), RequestHeaderAuthenticationFilter.class).authorizeRequests()
+					.antMatchers("/login**").permitAll().and().formLogin().permitAll().and().logout().permitAll().and()
+					.csrf().disable().exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+						@Override
+						public void commence(HttpServletRequest request, HttpServletResponse response,
+								AuthenticationException authException) throws IOException, ServletException {
+							// Pre-authenticated entry point called. Rejecting access
+							response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+						}
+					}).and().headers().frameOptions().disable();
+		}
+
+		@Bean
+		@Override
+		protected AuthenticationManager authenticationManager() {
+			final List<AuthenticationProvider> providers = new ArrayList<>(2);
+			providers.add(preauthAuthProvider());
+			providers.add(authProvider);
+			return new ProviderManager(providers);
+		}
+
+		@Bean(name = "certFilter")
+		PreauthenticationFilter certFilter() {
+			PreauthenticationFilter ret = new PreauthenticationFilter();
+			ret.setAuthenticationManager(authenticationManager());
+			return ret;
+		}
+
+		@Bean(name = "preAuthProvider")
+		PreAuthenticatedAuthenticationProvider preauthAuthProvider() {
+			PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+			provider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
+			return provider;
+		}
+
+		@Bean
+		UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() {
+			UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper = new UserDetailsByNameServiceWrapper<>();
+			wrapper.setUserDetailsService(customUserDetailsService);
+			return wrapper;
+		}
+	}
+
+	@Profile("remote")
+	@EnableWebSecurity
+	@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+	public static class RemoteSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		@Override
-		public void commence(HttpServletRequest request, HttpServletResponse response,
-				AuthenticationException authException) throws IOException, ServletException {
-			// Pre-authenticated entry point called. Rejecting access
-	        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeHttpRequests(
+					authorize -> authorize.mvcMatchers("/api/**").authenticated().mvcMatchers("/**").permitAll())
+					.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 		}
-    	  
-      })
-      .and()
-      .headers().frameOptions().disable();
-  }
-
-	@Bean
-	@Override
-	protected AuthenticationManager authenticationManager()  {
-		final List<AuthenticationProvider> providers = new ArrayList<>(2);
-		providers.add(preauthAuthProvider());
-		providers.add(authProvider);
-		return new ProviderManager(providers);
 	}
 
-  	@Bean(name="certFilter")
-  	PreauthenticationFilter certFilter() {
-  		PreauthenticationFilter ret = new PreauthenticationFilter();
-		ret.setAuthenticationManager(authenticationManager());
-  		return ret;
-  	}
-  
-	@Bean(name = "preAuthProvider")
-	PreAuthenticatedAuthenticationProvider preauthAuthProvider()  {
-		PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
-		provider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
-		return provider;
-	}
-
-	@Bean
-	UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper()  {
-		UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper = new UserDetailsByNameServiceWrapper<>();
-		wrapper.setUserDetailsService(customUserDetailsService);
-		return wrapper;
-	}
-
-    
 }
