b443b976f8db3c798207df516680f7960222307c
Kristina <kristina.sanchez@bc3research.org>
2024-10-23 11:17:14 +0200

    chore: invalid changes after merging Keycloak updates into develop
    - Made adjustments for compatibility with the old branch, but the changes are invalid and need further review for compilation and testing.

--- klab.hub/src/main/java/org/integratedmodelling/klab/hub/repository/UserRepository.java
+++ klab.hub/src/main/java/org/integratedmodelling/klab/hub/repository/UserRepository.java
@@ -14,96 +14,51 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends ResourceRepository<User, String>{		
-	
-	Optional<User> findById(String id);
-	
-	Optional<User> findByName(String username); // need exactly the username
-	
-	Optional<User> findByNameIgnoreCase(String username);
-	
-	Optional<User> findByNameIgnoreCaseOrderByNameAsc(String username);
-	
-	Optional<User> findByEmailIgnoreCase(String email);
-	
-	Optional<User> findByNameIgnoreCaseOrEmailIgnoreCase(String username, String email);
-	
-	Collection<User> findByNameInIgnoreCase(Collection<String> username);
-	
+public interface UserRepository extends ResourceRepository<User, String> {
+
+    Optional<User> findById(String id);
+
+    Optional<User> findByName(String username); // need exactly the username
+
+    Optional<User> findByNameIgnoreCase(String username);
+
+    Optional<User> findByNameIgnoreCaseOrderByNameAsc(String username);
+
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    Optional<User> findByNameIgnoreCaseOrEmailIgnoreCase(String username, String email);
+
+    Collection<User> findByNameInIgnoreCase(Collection<String> username);
+
     Boolean existsByNameIgnoreCase(String username);
 
     Boolean existsByEmailIgnoreCase(String email);
-    
-    
-    @Aggregation(pipeline = {
-    	  "{"
-   		+ "    $group: {"
-   		+ "      _id: {"
-		+ "      	month: {"
-		+ "       	 $month: $registrationDate"
-		+ "      	},"
-		+ "     	 year: {"
-		+ "      	  $year: $registrationDate"
-		+ "    	  	}"
-		+ "      },"
-   		+ "      count: {"
-   		+ "        $sum: 1"
-   		+ "      },"
-   		+ "		month:{"
-   		+ "			$first: {$month: $registrationDate}"
-   		+ "		},"
-   		+ "		year:{"
-   		+ "			$first: {$year: $registrationDate}"
-   		+ "    },"
-		+ "    dateString: {"
-   		+ "			$first: {"
-   		+ "				 $dateToString: {"
-   		+ "    				date: $registrationDate,"
-   		+ "    				format: '%Y-%m'"
-   		+ "				}"
-   		+ "			}"
-		+ "    	  }"
-   		+ "    }"
-   		+ "  }"
-	})
-    
+
+    @Aggregation(pipeline = {"{" + "    $group: {" + "      _id: {" + "      	month: {"
+            + "       	 $month: $registrationDate" + "      	}," + "     	 year: {" + "      	  $year: $registrationDate"
+            + "    	  	}" + "      }," + "      count: {" + "        $sum: 1" + "      }," + "		month:{"
+            + "			$first: {$month: $registrationDate}" + "		}," + "		year:{"
+            + "			$first: {$year: $registrationDate}" + "    }," + "    dateString: {" + "			$first: {"
+            + "				 $dateToString: {" + "    				date: $registrationDate,"
+            + "    				format: '%Y-%m'" + "				}" + "			}" + "    	  }" + "    }" + "  }"})
+
     public List<GroupUsersByDate> groupByMonthYear();
-    
-    @Aggregation(pipeline = {
-    		"{"
-   		+ "    $group: {"
-   		+ "      _id: {"
-		+ "     	 year: {"
-		+ "      	  $year: $registrationDate"
-		+ "    	  	}"
-		+ "      },"
-   		+ "      count: {"
-   		+ "        $sum: 1"
-   		+ "      },"
-   		+ "		year:{"
-   		+ "			$first: {$year: $registrationDate}"
-   		+ "    },"
-		+ "    dateString: {"
-   		+ "			'$first': {"
-   		+ "				 '$dateToString': {"
-   		+ "    				'date': '$registrationDate',"
-   		+ "    				'format': '%Y'"
-   		+ "				}"
-   		+ "			}"
-		+ "    	  }"
-   		+ "    }"
-   		+ "  }"
-	})
-    
+
+    @Aggregation(pipeline = {"{" + "    $group: {" + "      _id: {" + "     	 year: {" + "      	  $year: $registrationDate"
+            + "    	  	}" + "      }," + "      count: {" + "        $sum: 1" + "      }," + "		year:{"
+            + "			$first: {$year: $registrationDate}" + "    }," + "    dateString: {" + "			'$first': {"
+            + "				 '$dateToString': {" + "    				'date': '$registrationDate',"
+            + "    				'format': '%Y'" + "				}" + "			}" + "    	  }" + "    }" + "  }"})
+
     public List<GroupUsersByDate> groupByYear();
-    
+
     @Query("{'groupEntries.group.$id' : ?0}")
-	List<User> getUsersByGroupEntriesWithGroupId(ObjectId id);
+    List<User> getUsersByGroupEntriesWithGroupId(ObjectId id);
 
     @Query("{'roles' : ?0}")
     List<User> getUsersByRole(Role role);
 
     @Query("{'accountStatus' : ?0}")
-	List<User> getUsersByAccountStatus(AccountStatus accountStatus);
-    
+    List<User> getUsersByAccountStatus(AccountStatus accountStatus);
+
 }
--- klab.hub/src/main/java/org/integratedmodelling/klab/hub/tokens/services/UserAuthTokenServiceImpl.java
+++ klab.hub/src/main/java/org/integratedmodelling/klab/hub/tokens/services/UserAuthTokenServiceImpl.java
@@ -1,97 +1,96 @@
-//package org.integratedmodelling.klab.hub.tokens.services;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import org.integratedmodelling.klab.exceptions.KlabException;
-//import org.integratedmodelling.klab.hub.licenses.dto.JwtToken;
-//import org.integratedmodelling.klab.hub.payload.EngineProfileResource;
-//import org.integratedmodelling.klab.hub.repository.TokenRepository;
-//import org.integratedmodelling.klab.hub.repository.UserRepository;
-//import org.integratedmodelling.klab.hub.tokens.commands.CreateUserAuthenticationToken;
-//import org.integratedmodelling.klab.hub.tokens.commands.DeleteAuthenticationToken;
-//import org.integratedmodelling.klab.hub.tokens.dto.TokenAuthentication;
-//import org.integratedmodelling.klab.hub.tokens.enums.TokenType;
-//import org.integratedmodelling.klab.hub.tokens.exceptions.AuthenticationFailedException;
-//import org.integratedmodelling.klab.hub.users.commands.GetUserProfile;
-//import org.integratedmodelling.klab.hub.users.dto.ProfileResource;
-//import org.integratedmodelling.klab.hub.users.dto.User;
-//import org.integratedmodelling.klab.hub.users.exceptions.LoginFailedExcepetion;
-//import org.integratedmodelling.klab.hub.users.payload.LoginResponse;
-//import org.integratedmodelling.klab.hub.users.payload.LogoutResponse;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-//import org.springframework.stereotype.Service;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.mongodb.MongoException;
-//
-//@Service
-//public class UserAuthTokenServiceImpl implements UserAuthTokenService {
-//
-//    protected static final Logger logger = LoggerFactory.getLogger(UserAuthTokenServiceImpl.class);
-//
-//    private AuthenticationManager authenticationManager;
-//
-//    private UserRepository userRepository;
-//
-//    private TokenRepository tokenRepository;
-//
-//    private ObjectMapper objectMapper;
-//
-//    public UserAuthTokenServiceImpl(AuthenticationManager authenticationManager, UserRepository userRepository,
-//            TokenRepository tokenRepository, ObjectMapper objectMapper) {
-//        super();
-//        this.authenticationManager = authenticationManager;
-//        this.userRepository = userRepository;
-//        this.tokenRepository = tokenRepository;
-//        this.objectMapper = objectMapper;
-//    }
-//
-//    private static final JwtToken JWT_TOKEN_FACTORY = new JwtToken();
-//
-//    @Override
-//    public TokenAuthentication createToken(String username, TokenType type) {
-//        if (TokenType.auth == type) {
-//            TokenAuthentication token = null;
-//            Optional<User> user = userRepository.findByNameIgnoreCase(username);
-//            if (user.isPresent()) {
-//                token = new CreateUserAuthenticationToken(tokenRepository, user.get()).execute();
-//            }
-//            return token;
-//        } else {
-//            throw new AuthenticationFailedException("Incorrect workflow for creating token");
-//        }
-//    }
-//
-//    @Override
-//    public void deleteToken(String tokenString) {
-//        try {
-//            new DeleteAuthenticationToken(tokenRepository, tokenString).execute();
-//        } catch (MongoException e) {
-//            logger.error(e.getMessage(), e);
-//            throw new KlabException("Error deleting token, " + e.getMessage(), e);
-//        }
-//    }
-//
-//    private void deleteExpiredTokens(String username) {
-//        List<TokenAuthentication> dbTokens = tokenRepository.findByUsername(username);
-//        for(TokenAuthentication dbToken : dbTokens) {
-//            if (dbToken.isExpired()) {
-//                deleteToken(dbToken.getTokenString());
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public TokenAuthentication getUserAuthenticationToken(String username, String password) {
-//        Authentication authRequest = new UsernamePasswordAuthenticationToken(username, password);
+package org.integratedmodelling.klab.hub.tokens.services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.integratedmodelling.klab.exceptions.KlabException;
+import org.integratedmodelling.klab.hub.licenses.dto.JwtToken;
+import org.integratedmodelling.klab.hub.payload.EngineProfileResource;
+import org.integratedmodelling.klab.hub.repository.TokenRepository;
+import org.integratedmodelling.klab.hub.repository.UserRepository;
+import org.integratedmodelling.klab.hub.tokens.commands.CreateUserAuthenticationToken;
+import org.integratedmodelling.klab.hub.tokens.commands.DeleteAuthenticationToken;
+import org.integratedmodelling.klab.hub.tokens.dto.TokenAuthentication;
+import org.integratedmodelling.klab.hub.tokens.enums.TokenType;
+import org.integratedmodelling.klab.hub.tokens.exceptions.AuthenticationFailedException;
+import org.integratedmodelling.klab.hub.users.commands.GetUserProfile;
+import org.integratedmodelling.klab.hub.users.dto.ProfileResource;
+import org.integratedmodelling.klab.hub.users.dto.User;
+import org.integratedmodelling.klab.hub.users.payload.LoginResponse;
+import org.integratedmodelling.klab.hub.users.payload.LogoutResponse;
+import org.integratedmodelling.klab.hub.users.services.UserProfileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.MongoException;
+
+@Service
+public class UserAuthTokenServiceImpl implements UserAuthTokenService {
+
+    protected static final Logger logger = LoggerFactory.getLogger(UserAuthTokenServiceImpl.class);
+
+    private UserRepository userRepository;
+
+    private UserProfileService profileService;
+
+    private TokenRepository tokenRepository;
+
+    private ObjectMapper objectMapper;
+
+    public UserAuthTokenServiceImpl(UserRepository userRepository, UserProfileService profileService,
+            TokenRepository tokenRepository, ObjectMapper objectMapper) {
+        super();
+        this.userRepository = userRepository;
+        this.profileService = profileService;
+        this.tokenRepository = tokenRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    private static final JwtToken JWT_TOKEN_FACTORY = new JwtToken();
+
+    @Override
+    public TokenAuthentication createToken(String username, TokenType type) {
+        if (TokenType.auth == type) {
+            TokenAuthentication token = null;
+            Optional<User> user = userRepository.findByNameIgnoreCase(username);
+            if (user.isPresent()) {
+                token = new CreateUserAuthenticationToken(tokenRepository, user.get()).execute();
+            }
+            return token;
+        } else {
+            throw new AuthenticationFailedException("Incorrect workflow for creating token");
+        }
+    }
+
+    @Override
+    public void deleteToken(String tokenString) {
+        try {
+            new DeleteAuthenticationToken(tokenRepository, tokenString).execute();
+        } catch (MongoException e) {
+            logger.error(e.getMessage(), e);
+            throw new KlabException("Error deleting token, " + e.getMessage(), e);
+        }
+    }
+
+    private void deleteExpiredTokens(String username) {
+        List<TokenAuthentication> dbTokens = tokenRepository.findByUsername(username);
+        for(TokenAuthentication dbToken : dbTokens) {
+            if (dbToken.isExpired()) {
+                deleteToken(dbToken.getTokenString());
+            }
+        }
+    }
+
+    @Override
+    public TokenAuthentication getUserAuthenticationToken(String username, String password) {
+        Authentication authRequest = new UsernamePasswordAuthenticationToken(username, password);
 //        try {
 //            authRequest = authenticationManager.authenticate(authRequest);
 //        } catch (AuthenticationException e) {
@@ -101,33 +100,35 @@
 //            String msg = "Something went wrong with authentication. Result.isAuthenticated() == false, but no exception was thrown.";
 //            throw new KlabException(msg);
 //        } else {
-//            deleteExpiredTokens(username);
-//            TokenAuthentication result = createToken(username, TokenType.auth);
-//            PreAuthenticatedAuthenticationToken secureToken = new PreAuthenticatedAuthenticationToken(result.getPrincipal(),
-//                    result.getCredentials(), result.getAuthorities());
-//            SecurityContextHolder.getContext().setAuthentication(secureToken);
-//            return result;
+        deleteExpiredTokens(username);
+        TokenAuthentication result = createToken(username, TokenType.auth);
+        PreAuthenticatedAuthenticationToken secureToken = new PreAuthenticatedAuthenticationToken(result.getPrincipal(),
+                result.getCredentials(), result.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(secureToken);
+        return result;
 //        }
-//    }
-//
-//    @SuppressWarnings("rawtypes")
-//    @Override
-//    public LoginResponse< ? > getAuthResponse(String username, String password, boolean remote) {
-//        TokenAuthentication token = getUserAuthenticationToken(username, password);
-//        ProfileResource profile = new GetUserProfile(userRepository, username, objectMapper).execute();
-//        if (remote) {
-//            profile.setJwtToken(JWT_TOKEN_FACTORY.createEngineJwtToken(profile));
-//            return new LoginResponse<EngineProfileResource>(token, new EngineProfileResource(profile));
-//        } else {
-//            return new LoginResponse<ProfileResource>(token, profile.getSafeProfile());
-//        }
-//    }
-//
-//    @Override
-//    public LogoutResponse getLogoutResponse(String token) {
-//        deleteToken(token);
-//        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-//        return new LogoutResponse(username);
-//    }
-//
-//}
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public LoginResponse< ? > getAuthResponse(String username, String password, boolean remote) {
+        TokenAuthentication token = getUserAuthenticationToken(username, password);
+        ProfileResource profile = new GetUserProfile(userRepository, username, objectMapper).execute();
+        profile.setLastLogin(LocalDateTime.now());
+        profileService.updateUserByProfile(profile);
+        if (remote) {
+            profile.setJwtToken(JWT_TOKEN_FACTORY.createEngineJwtToken(profile));
+            return new LoginResponse<EngineProfileResource>(token, new EngineProfileResource(profile));
+        } else {
+            return new LoginResponse<ProfileResource>(token, profile.getSafeProfile());
+        }
+    }
+
+    @Override
+    public LogoutResponse getLogoutResponse(String token) {
+        deleteToken(token);
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return new LogoutResponse(username);
+    }
+
+}
