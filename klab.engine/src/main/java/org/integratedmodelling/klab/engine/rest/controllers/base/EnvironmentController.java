509045eb793bc365cd852c2d5d18739c851ebc9f
Kristina <kristina.sanchez@bc3research.org>
2024-12-02 13:17:42 +0100

    IM-457 Add activeProfile to environments controlles to use in k.explorer

--- klab.engine/src/main/java/org/integratedmodelling/klab/engine/rest/controllers/base/EnvironmentController.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/engine/rest/controllers/base/EnvironmentController.java
@@ -2,6 +2,7 @@ package org.integratedmodelling.klab.engine.rest.controllers.base;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,14 +22,22 @@ public class EnvironmentController {
 
     private static final String APP_BASE_URL = "APP_BASE_URL";
     private static final String KEYCLOAK_URL = "KEYCLOAK_URL";
+    private static final String ACTIVE_PROFILE = "ACTIVE_PROFILE";
 
     @GetMapping(value = "/engine/environments")
     public void getEnvironmentVariables(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         response.setContentType("text/javascript;utf-8");
+        
+        String activeProfile =  System.getProperty("spring.profiles.active", "engine.local");
 
+        /* 
+         * Get engine properties
+         */
         Map<String, String> kHubEnvironmentVariables = Map.ofEntries(Map.entry(APP_BASE_URL, engineProperties.env.getAppBaseUrl()),
-                Map.entry(KEYCLOAK_URL, engineProperties.env.getKeycloakUrl()));
+                Map.entry(KEYCLOAK_URL, engineProperties.env.getKeycloakUrl()), Map.entry(ACTIVE_PROFILE, activeProfile));
+        
+        
 
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonValue = objectMapper.writeValueAsString(kHubEnvironmentVariables);
