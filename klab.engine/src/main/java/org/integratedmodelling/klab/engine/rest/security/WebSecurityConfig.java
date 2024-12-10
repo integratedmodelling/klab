fe769359ef1658d9a3be5f1853628bbb0c35d956
Kristina <kristina.sanchez@bc3research.org>
2024-12-10 15:42:42 +0100

    IM-457 Add logger to try to see an error and format files. docker build

--- klab.engine/src/main/java/org/integratedmodelling/klab/engine/rest/security/WebSecurityConfig.java
+++ klab.engine/src/main/java/org/integratedmodelling/klab/engine/rest/security/WebSecurityConfig.java
@@ -3,9 +3,11 @@ package org.integratedmodelling.klab.engine.rest.security;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.servlet.ServletException;
@@ -17,6 +19,8 @@ import org.integratedmodelling.klab.api.auth.IIdentity;
 import org.integratedmodelling.klab.api.auth.KlabHttpHeaders;
 import org.integratedmodelling.klab.api.auth.Roles;
 import org.integratedmodelling.klab.engine.rest.utils.EngineHttpUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,163 +44,172 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 class WebSecurityConfig {
+    
+    private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
 
-	interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {
-	}
+    interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> {
+    }
 
-	@Profile("engine.remote")
-	@EnableWebSecurity
-	@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-	class WebSecurityConfigRemote extends WebSecurityConfigurerAdapter {
+    @Profile("engine.remote")
+    @EnableWebSecurity
+    @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+    class WebSecurityConfigRemote extends WebSecurityConfigurerAdapter {
 
-		@Autowired
-		private PreauthenticatedUserDetailsService customUserDetailsService;
+        @Autowired
+        private PreauthenticatedUserDetailsService customUserDetailsService;
 
-		@Bean
-		AuthoritiesConverter realmRolesAuthoritiesConverter() {
-			return claims -> {
-				final var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
-				final var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
+        @Bean
+        AuthoritiesConverter realmRolesAuthoritiesConverter() {
+            return claims -> {
+                final var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
+                final var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
 
-				roles.ifPresent(role -> role.add(Roles.PUBLIC));
+                roles.ifPresent(role -> role.add(Roles.PUBLIC));
 
-				HttpServletRequest request = EngineHttpUtils.getCurrentHttpRequest();
+                HttpServletRequest request = EngineHttpUtils.getCurrentHttpRequest();
+                
+                Map<String, String> headers = Collections.list(request.getHeaderNames())
+                        .stream()
+                        .collect(Collectors.toMap(h -> h, request::getHeader));
+                
+                logger.info(headers.toString().replace(',', '\n'));
+                
 
-				String klabAuth = request.getHeader(KlabHttpHeaders.KLAB_AUTHORIZATION);
+                String klabAuth = request.getHeader(KlabHttpHeaders.KLAB_AUTHORIZATION);
+                
+                logger.info(klabAuth);
 
-				if (klabAuth != null) {
-					// send anything already known downstream
-					if (Authentication.INSTANCE.getIdentity(klabAuth, IIdentity.class) != null) {
-						roles.ifPresent(role -> role.add(Roles.SESSION));
-					}
-				}
+                if (klabAuth != null) {
+                    // send anything already known downstream
+                    if (Authentication.INSTANCE.getIdentity(klabAuth, IIdentity.class) != null) {
+                        logger.info("Add ROLE_SESSION");
+                        roles.ifPresent(role -> role.add(Roles.SESSION));
+                    }
+                }
 
-				List<GrantedAuthority> rolesList = roles.map(List::stream).orElse(Stream.empty())
-						.map(SimpleGrantedAuthority::new).map(GrantedAuthority.class::cast).toList();
+                List<GrantedAuthority> rolesList = roles.map(List::stream).orElse(Stream.empty()).map(SimpleGrantedAuthority::new)
+                        .map(GrantedAuthority.class::cast).toList();
 
-				return rolesList;
-			};
-		}
+                return rolesList;
+            };
+        }
 
-		@Bean
-		JwtAuthenticationConverter authenticationConverter(
-				Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter) {
-			JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-			jwtAuthenticationConverter
-					.setJwtGrantedAuthoritiesConverter(jwt -> authoritiesConverter.convert(jwt.getClaims()));
-			return jwtAuthenticationConverter;
-		}
+        @Bean
+        JwtAuthenticationConverter authenticationConverter(
+                Converter<Map<String, Object>, Collection<GrantedAuthority>> authoritiesConverter) {
+            JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+            jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> authoritiesConverter.convert(jwt.getClaims()));
+            return jwtAuthenticationConverter;
+        }
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
 
-			http.cors().and().csrf().disable().addFilterAfter(certFilter(), RequestHeaderAuthenticationFilter.class)
-					.authorizeHttpRequests(authorize -> authorize.mvcMatchers("/api/**").authenticated()
-							.mvcMatchers("/**").permitAll())
-					.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-		}
+            http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .addFilterAfter(certFilter(), RequestHeaderAuthenticationFilter.class)
+                    .authorizeHttpRequests(
+                            authorize -> authorize.mvcMatchers("/api/**").authenticated().mvcMatchers("/**").permitAll())
+                    .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        }
 
-		@Bean
-		@Override
-		protected AuthenticationManager authenticationManager() {
-			final List<AuthenticationProvider> providers = new ArrayList<>(2);
-			providers.add(preauthAuthProvider());
-			return new ProviderManager(providers);
-		}
+        @Bean
+        @Override
+        protected AuthenticationManager authenticationManager() {
+            final List<AuthenticationProvider> providers = new ArrayList<>(2);
+            providers.add(preauthAuthProvider());
+            return new ProviderManager(providers);
+        }
 
-		@Bean(name = "certFilter")
-		PreauthenticationFilter certFilter() {
-			PreauthenticationFilter ret = new PreauthenticationFilter();
-			ret.setAuthenticationManager(authenticationManager());
-			return ret;
-		}
+        @Bean(name = "certFilter")
+        PreauthenticationFilter certFilter() {
+            PreauthenticationFilter ret = new PreauthenticationFilter();
+            ret.setAuthenticationManager(authenticationManager());
+            return ret;
+        }
 
-		@Bean(name = "preAuthProvider")
-		PreAuthenticatedAuthenticationProvider preauthAuthProvider() {
-			PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
-			provider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
-			return provider;
-		}
+        @Bean(name = "preAuthProvider")
+        PreAuthenticatedAuthenticationProvider preauthAuthProvider() {
+            PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+            provider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
+            return provider;
+        }
 
-		@Bean
-		UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() {
-			UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper = new UserDetailsByNameServiceWrapper<>();
-			wrapper.setUserDetailsService(customUserDetailsService);
-			return wrapper;
-		}
+        @Bean
+        UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() {
+            UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper = new UserDetailsByNameServiceWrapper<>();
+            wrapper.setUserDetailsService(customUserDetailsService);
+            return wrapper;
+        }
 
-	}
+    }
 
-	@Profile("engine.local")
-	@EnableWebSecurity
-	@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-	class WebSecurityConfigLocal extends WebSecurityConfigurerAdapter {
+    @Profile("engine.local")
+    @EnableWebSecurity
+    @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+    class WebSecurityConfigLocal extends WebSecurityConfigurerAdapter {
 
-		@Autowired
-		private PreauthenticatedUserDetailsService customUserDetailsService;
+        @Autowired
+        private PreauthenticatedUserDetailsService customUserDetailsService;
 
-		@Autowired
-		private EngineDirectoryAuthenticationProvider authProvider;
+        @Autowired
+        private EngineDirectoryAuthenticationProvider authProvider;
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-					// disable automatic session creation to avoid use of cookie session
-					// and the consequent authentication failures in web ui
-					.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-					.addFilterBefore(certFilter(), RequestHeaderAuthenticationFilter.class)
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    // disable automatic session creation to avoid use of cookie session
+                    // and the consequent authentication failures in web ui
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                    .addFilterBefore(certFilter(), RequestHeaderAuthenticationFilter.class)
 //		  .authorizeRequests().anyRequest().hasAnyRole("ADMIN")
 //	      .and()
-					.authorizeRequests().antMatchers("/login**").permitAll().and().formLogin().permitAll().and()
-					.logout().permitAll().and().csrf().disable().exceptionHandling()
-					.authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    .authorizeRequests().antMatchers("/login**").permitAll().and().formLogin().permitAll().and().logout()
+                    .permitAll().and().csrf().disable().exceptionHandling()
+                    .authenticationEntryPoint(new AuthenticationEntryPoint(){
 
-						@Override
-						public void commence(HttpServletRequest request, HttpServletResponse response,
-								AuthenticationException authException) throws IOException, ServletException {
-							// Pre-authenticated entry point called. Rejecting access
-							response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-						}
+                        @Override
+                        public void commence(HttpServletRequest request, HttpServletResponse response,
+                                AuthenticationException authException) throws IOException, ServletException {
+                            // Pre-authenticated entry point called. Rejecting access
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                        }
 
-					}).and().headers().frameOptions().disable();
-		}
+                    }).and().headers().frameOptions().disable();
+        }
 
-		@Bean
-		@Override
-		protected AuthenticationManager authenticationManager() {
-			final List<AuthenticationProvider> providers = new ArrayList<>(2);
-			providers.add(preauthAuthProvider());
-			providers.add(authProvider);
-			return new ProviderManager(providers);
-		}
+        @Bean
+        @Override
+        protected AuthenticationManager authenticationManager() {
+            final List<AuthenticationProvider> providers = new ArrayList<>(2);
+            providers.add(preauthAuthProvider());
+            providers.add(authProvider);
+            return new ProviderManager(providers);
+        }
 
-		@Bean(name = "certFilter")
-		PreauthenticationFilter certFilter() {
-			PreauthenticationFilter ret = new PreauthenticationFilter();
-			ret.setAuthenticationManager(authenticationManager());
-			return ret;
-		}
+        @Bean(name = "certFilter")
+        PreauthenticationFilter certFilter() {
+            PreauthenticationFilter ret = new PreauthenticationFilter();
+            ret.setAuthenticationManager(authenticationManager());
+            return ret;
+        }
 
-		@Bean(name = "preAuthProvider")
-		PreAuthenticatedAuthenticationProvider preauthAuthProvider() {
-			PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
-			provider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
-			return provider;
-		}
+        @Bean(name = "preAuthProvider")
+        PreAuthenticatedAuthenticationProvider preauthAuthProvider() {
+            PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+            provider.setPreAuthenticatedUserDetailsService(userDetailsServiceWrapper());
+            return provider;
+        }
 
-		@Bean
-		UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() {
-			UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper = new UserDetailsByNameServiceWrapper<>();
-			wrapper.setUserDetailsService(customUserDetailsService);
-			return wrapper;
-		}
+        @Bean
+        UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> userDetailsServiceWrapper() {
+            UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken> wrapper = new UserDetailsByNameServiceWrapper<>();
+            wrapper.setUserDetailsService(customUserDetailsService);
+            return wrapper;
+        }
 
-	}
+    }
 
 }
--- products/cloud/src/main/java/org/integratedmodelling/klab/engine/services/HubUserService.java
+++ products/cloud/src/main/java/org/integratedmodelling/klab/engine/services/HubUserService.java
@@ -70,7 +70,7 @@ public class HubUserService implements RemoteUserService {
     @Override
     public ResponseEntity< ? > login(RemoteUserAuthenticationRequest login) {
         ResponseEntity<HubLoginResponse> result = null;
-        if (!"".equals(login.getUsername()) && null==login.getToken()) {
+        if (!"".equals(login.getUsername()) && null == login.getToken()) {
             login.setRemote(true);
             try {
                 result = hubLogin(login);
@@ -84,7 +84,7 @@ public class HubUserService implements RemoteUserService {
                 HubUserProfile profile = result.getBody().getProfile();
                 String authToken = result.getBody().getAuthentication().getTokenString();
                 profile.setAuthToken(authToken);
-                
+
                 RemoteUserLoginResponse response = getLoginResponse(profile, null);
                 response.setAuthorization(authToken);
 
@@ -272,30 +272,35 @@ public class HubUserService implements RemoteUserService {
     }
 
     private ResponseEntity<HubLoginResponse> hubLogin(UserAuthenticationRequest login) {
-    	HttpEntity< ? > request = new HttpEntity<>(login);
-        
-        String authorization = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization");
-        
+        HttpHeaders headers = new HttpHeaders();
+        String authorization = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest()
+                .getHeader("Authorization");
+
         if (authorization != null) {
-        	HttpHeaders headers = new HttpHeaders();
-        	headers.add("Authorization", ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization"));        	
+            
+            headers.add("Authorization", ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest()
+                    .getHeader("Authorization"));
         }
-                
+
+        HttpEntity< ? > request = new HttpEntity<>(login, headers);
+
         return restTemplate.postForEntity(getLoginUrl(), request, HubLoginResponse.class);
     }
 
     private URI hubLogout(String token) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authentication", token);
-        headers.add("Authorization", ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization"));
+        headers.add("Authorization",
+                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization"));
         HttpEntity< ? > request = new HttpEntity<>(headers);
         return restTemplate.postForLocation(getLogOutUrll(), request);
     }
 
     private ResponseEntity<HubUserProfile> hubToken(String token) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authentication", token);        
-        headers.add("Authorization", ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization"));
+        headers.add("Authentication", token);
+        headers.add("Authorization",
+                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization"));
         HttpEntity< ? > request = new HttpEntity<>(headers);
         ResponseEntity<HubUserProfile> response = restTemplate.exchange(getProfileUrl(), HttpMethod.GET, request,
                 HubUserProfile.class, true);
