b6d48e14dd569f0eee3afd52b5d5f37b9c67e0b2
Kristina <kristina.sanchez@bc3research.org>
2025-01-08 15:22:21 +0100

    test: Testing error /ui

--- klab.hub/src/main/java/org/integratedmodelling/klab/hub/controllers/FrontEndController.java
+++ klab.hub/src/main/java/org/integratedmodelling/klab/hub/controllers/FrontEndController.java
@@ -1,18 +1,18 @@
-package org.integratedmodelling.klab.hub.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-@RequestMapping("/")
-public class FrontEndController {
-
-    @GetMapping("/")
-    public ModelAndView home() {
-    	ModelAndView home = new ModelAndView();
-    	home.setViewName("/ui/index.html");
-    	return home;
-    }
-}
+//package org.integratedmodelling.klab.hub.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//@RequestMapping("/")
+//public class FrontEndController {
+//
+//    @GetMapping("/")
+//    public ModelAndView home() {
+//        ModelAndView home = new ModelAndView();
+//        home.setViewName("/ui/index.html");
+//        return home;
+//    }
+//}
