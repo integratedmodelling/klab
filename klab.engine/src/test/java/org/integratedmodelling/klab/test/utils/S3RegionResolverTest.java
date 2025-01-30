94b743649d6d709c64e0be1a02c06883d048091d
Íñigo Cobián <inigo.cobian@gmail.com>
2025-01-30 17:16:02 +0100

    Update S3RegionResolverTest
    Added mock for Monitor

--- klab.engine/src/test/java/org/integratedmodelling/klab/test/utils/S3RegionResolverTest.java
+++ klab.engine/src/test/java/org/integratedmodelling/klab/test/utils/S3RegionResolverTest.java
@@ -1,20 +1,22 @@
 package org.integratedmodelling.klab.test.utils;
 
+import org.integratedmodelling.klab.api.runtime.monitoring.IMonitor;
 import org.integratedmodelling.klab.exceptions.KlabResourceAccessException;
 import org.integratedmodelling.klab.utils.s3.S3RegionResolver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import software.amazon.awssdk.regions.Region;
 
 public class S3RegionResolverTest {
-
     @Test
     public void resolveBucketRegion_resolutionSuccessful() {
         String bucket = "deafrica-input-datasets";
         String objectKey = "rainfall_chirps_monthly/chirps-v2.0_2024.03.tif";
+        IMonitor monitor = Mockito.mock(IMonitor.class);
 
-        Region ret = S3RegionResolver.resolveBucketRegion(bucket, objectKey);
+        Region ret = S3RegionResolver.resolveBucketRegion(bucket, objectKey, monitor);
 
         Assertions.assertEquals(Region.AF_SOUTH_1, ret);
     }
@@ -24,9 +26,10 @@ public class S3RegionResolverTest {
     public void resolveBucketRegion_resolutionUnsuccessful() {
         String bucket = "fake-bucket";
         String objectKey = "fake-object.tif";
+        IMonitor monitor = Mockito.mock(IMonitor.class);
 
         Assertions.assertThrows(KlabResourceAccessException.class, () -> {
-            S3RegionResolver.resolveBucketRegion(bucket, objectKey);
+            S3RegionResolver.resolveBucketRegion(bucket, objectKey, monitor);
         });
     }
 
