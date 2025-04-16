251fb5de5c07154d61102601a8cec8aaf76eea8e
Kristina <kristina.sanchez@bc3research.org>
2025-04-16 13:00:45 +0200

    IM-674 Add logic to check for missing secret in JSON and throw exception
    - Implemented logic to check if the 'secret' key exists in the JSON object before accessing its value.
    - Added a check to handle the case where the key is missing, throwing a HubHttpException with a specific error message and HTTP status.

--- klab.hub/src/main/java/org/integratedmodelling/klab/hub/vault/VaultService.java
+++ klab.hub/src/main/java/org/integratedmodelling/klab/hub/vault/VaultService.java
@@ -1,13 +1,17 @@
 package org.integratedmodelling.klab.hub.vault;
 
+import java.util.Map;
+
+import org.integratedmodelling.klab.hub.exception.HubHttpException;
 import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.support.VaultResponse;
 
 @Service
 public class VaultService {
-
+    
     private final VaultTemplate vaultTemplate;
 
     // Injecting VaultTemplate
@@ -22,12 +26,33 @@ public class VaultService {
         VaultResponse response = vaultTemplate.read(path);
         
         if (response == null || response.getData() == null) {
-            return null;
+            throw new HubHttpException(
+                    "vault.no_data_found", 
+                    "No data found at the requested path. " + path, 
+                    HttpStatus.NOT_FOUND
+                );
         }
 
         // Return the key 
-        JSONObject json = new JSONObject(response.getData().get("data"));
-        return (json.get(secret).toString());
+        Map data = (Map) response.getData().get("data");
+        if (data.isEmpty()) {
+            throw new HubHttpException(
+                    "vault.mapa_is_empty", 
+                    "The map is empty. No data found at the requested path. " + path, 
+                    HttpStatus.NOT_FOUND
+                );
+        }
+        
+        JSONObject json = new JSONObject((Map) data);
+        
+     // Check if the key 'secret' exists in the JSON object
+        if (json.has(secret)) {
+            return json.get(secret).toString();  // Get the value if the key exists
+        } else {
+            // Handle the case when 'secret' doesn't exist in the JSON
+            throw new HubHttpException(
+                    "vault.secret_not_found", "Secret not found", HttpStatus.NOT_FOUND);
+        }
     }
 }
 
